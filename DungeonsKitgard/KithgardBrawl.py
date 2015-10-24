6d5cf047292fe70cac56c5aab9bb885c7109035d
vadim-job-hg <vadim-job-hg@yandex.ru>
2015-10-24 20:34:31 +0300

    2

--- DungeonsKitgard/KithgardBrawl.py
+++ DungeonsKitgard/KithgardBrawl.py
@@ -1,5 +1,8 @@
 loop:
     enemy = self.findNearestEnemy()
+    item  = self.findNearestItem()
+    if item is not None:
+        self.moveXY(item.pos.x, item.pos.y)
     if enemy is not None:
         if self.isReady("power-up"):
             self.powerUp()
