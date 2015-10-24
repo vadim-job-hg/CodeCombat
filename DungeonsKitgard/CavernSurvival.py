72a69a6c8aa2a529357127a67e4862e027a574e3
vadim-job-hg <vadim-job-hg@yandex.ru>
2015-10-24 22:32:57 +0300

    2

---
+++ DungeonsKitgard/CavernSurvival.py
@@ -0,0 +1,12 @@
+loop:
+    enemy = self.findNearestEnemy()
+    item  = self.findNearestItem()
+    if item is not None:
+        self.moveXY(item.pos.x, item.pos.y)
+    if enemy is not None:
+        if self.isReady("power-up"):
+            self.powerUp()
+        elif self.isReady("bash"):
+            self.bash(enemy)
+        else:
+            self.attack(enemy)
