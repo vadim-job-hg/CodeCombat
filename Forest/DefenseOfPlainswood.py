f6d82883235d7b19cdb4233e7f7f6853bc140285
vadim-job-hg <vadim-job-hg@yandex.ru>
2015-10-25 09:15:42 +0200

    Forest

---
+++ Forest/DefenseOfPlainswood.py
@@ -0,0 +1,5 @@
+self.moveUp()
+self.buildXY("fence", 40, 52)
+self.moveDown()
+self.moveDown()
+self.buildXY("fence", 40, 24)
---
+++ Forest/ThumbBiter.py
@@ -0,0 +1,13 @@
+if (2 + 2 == 4):
+    self.say("Hey!")
+    
+if (2 + 2 == 4):
+    self.say("Yes, you!");
+
+
+if (3 + 3 == 6):
+    self.say("Come at me!");
+
+if (2* 10 == 20):
+    self.say("Prativnij");
+
