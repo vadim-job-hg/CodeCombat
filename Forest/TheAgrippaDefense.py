fe7bc08c3bc6362f6fb2bb929d23d2e973d77726
vadim-job-hg <vadim-job-hg@yandex.ru>
2015-10-26 09:13:48 +0200

    Forest

---
+++ Forest/TheAgrippaDefense.py
@@ -0,0 +1,10 @@
+loop:
+    enemy = self.findNearestEnemy()
+    if(enemy):
+        dist = self.distanceTo(enemy)
+        if(dist<5):
+            if(self.isReady("cleave")):        
+                self.cleave(enemy)        
+            else:
+                self.attack(enemy)
+    
