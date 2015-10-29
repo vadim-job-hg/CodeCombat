730108507190fbf4f80204c80ec0be849febfd9b
vadim-job-hg <vadim-job-hg@yandex.ru>
2015-10-29 20:46:34 +0200

    Forest

---
+++ Forest/MultiplayerTreasureGrove.py
@@ -0,0 +1,24 @@
+loop:
+    enemy = self.findNearestEnemy()
+    item  = self.findNearestItem()
+    dist_item = 9999999
+    dist_enemy= 99999999
+    if item:
+        dist_item = self.distanceTo(item)
+    if enemy:
+        dist_enemy = self.distanceTo(enemy)
+    if((dist_enemy<10) or (dist_item>dist_enemy)):
+         if(self.isReady("cleave")):
+            self.cleave(enemy)
+         elif(self.isReady("bash")):
+            self.bash(enemy)
+         elif(self.isReady("power-up")):
+            self.powerUp()
+         else:
+            self.shield()
+            self.attack(enemy)
+    elif(item):
+        self.moveXY(item.pos.x, item.pos.y)
+    else:
+        self.moveXY(42, 30)
+        
---
+++ Forest/SignalCorpse.py
@@ -0,0 +1,24 @@
+# You can use flags to choose different tactics.
+# In this level, the green flag will mean you want to move to the flag.
+# The black flag means you want to cleave at the flag.
+# The doctor will heal you at the Red X
+
+loop:
+    green = self.findFlag("green")
+    black = self.findFlag("black")
+    nearest = self.findNearestEnemy()
+    enemy = self.findNearestEnemy()
+    if enemy:
+        dist = self.distanceTo(enemy);
+    if green:
+        self.pickUpFlag(green)
+    elif black and self.isReady("cleave"):
+        self.pickUpFlag(black)
+        enemy = self.findNearestEnemy()
+        if enemy:
+            self.cleave(enemy);
+
+    elif nearest and self.distanceTo(nearest) < 10:
+        self.attack(nearest)
+        
+        pass
