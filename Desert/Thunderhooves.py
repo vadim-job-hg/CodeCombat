6fd8cfe1425e713827bdd225f539a56131520b9e
vadim-job-hg <vadim-job-hg@yandex.ru>
2015-10-29 23:04:09 +0200

    Desert

---
+++ Desert/Thunderhooves.py
@@ -0,0 +1,11 @@
+loop:
+    yak = self.findNearestEnemy()
+    if yak:        
+        if yak.pos.y>self.pos.y:
+            self.buildXY('fence', self.pos.x, yak.pos.y-10)
+        if yak.pos.y<self.pos.y:        
+            self.buildXY('fence', self.pos.x, yak.pos.y+10)
+        pass
+    else:
+        self.moveXY(self.pos.x+10, self.pos.y)
+        
