76a239ba560cd56682a4db583c9753335d2bf995
vadim.kuznetsov <22vadim76>
2015-10-30 19:11:53 +0200

    Forest

--- Desert/DesertCombat.py
+++ Desert/DesertCombat.py
@@ -1,1 +1,26 @@
-__author__ = 'Вадим'
+ordersGiven = 0
+while ordersGiven < 5:
+    x = self.pos.x
+    y = self.pos.y - 10
+    self.moveXY(x, y)
+    # Перемещайся и отдавай приказы каждому из союзников. (Они тебя слышат только если ты стоишь напротив них.)
+    self.say("Attack!")
+    ordersGiven +=1;
+    if(ordersGiven>=5):
+        break;
+self.moveXY(48, 31)
+enemy = self.findNearestEnemy()
+self.attack(enemy)
+self.attack(enemy)
+self.attack(enemy)
+self.attack(enemy)
+self.attack(enemy)
+self.attack(enemy)
+self.attack(enemy)
+self.attack(enemy)
+self.attack(enemy)
+self.attack(enemy)
+self.attack(enemy)
+self.attack(enemy)
+self.attack(enemy)
+
