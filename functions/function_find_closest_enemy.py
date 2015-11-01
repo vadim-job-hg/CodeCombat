ff75e8b74ab1f3f524d9a269d21e2a9c886466da
vadim-job-hg <vadim-job-hg@yandex.ru>
2015-11-01 21:30:14 +0200

    Mountain

--- functions/function_find_closest_enemy.py
+++ functions/function_find_closest_enemy.py
@@ -43,3 +43,30 @@ def tacktick():
         attack(nearest)
     else:
         pickUpNearestCoin()
+
+# Теперь у тебя есть Кольца Цветов! Ты можешь:
+# toggleFlowers(true/false) - включить или выключить.
+# setFlowerColor("random") - также можно выбрать "pink", "red", "blue", "purple", "yellow", или "white".
+# Вот некоторые функции для рисования фигур:
+# х, у - центр фигуры
+# size - размер фигуры (radius, side length)
+def drawCircle(x, y, size):
+    angle = 0
+    self.toggleFlowers(False)
+    while angle <= Math.PI * 2:
+        newX = x + (size * Math.cos(angle))
+        newY = y + (size * Math.sin(angle))
+        self.moveXY(newX, newY)
+        self.toggleFlowers(True)
+        angle += 0.2
+
+def drawSquare(x, y, size):
+    self.toggleFlowers(False)
+    cornerOffset = size / 2
+    self.moveXY(x - cornerOffset, y - cornerOffset)
+    self.toggleFlowers(True)
+    self.moveXY(x + cornerOffset, y - cornerOffset)
+    self.moveXY(x + cornerOffset, y + cornerOffset)
+    self.moveXY(x - cornerOffset, y + cornerOffset)
+    self.moveXY(x - cornerOffset, y - cornerOffset)
+
