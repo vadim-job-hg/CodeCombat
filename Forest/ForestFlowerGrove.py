6c56226dc8158515bb8af7465dc7fad93f1549ec
vadim-job-hg <vadim-job-hg@yandex.ru>
2015-11-01 22:12:16 +0200

    Mountain

---
+++ Forest/ForestFlowerGrove.py
@@ -0,0 +1,17 @@
+colors = ["pink", "red", "blue", "purple", "yellow", "white", "random"]
+index = len(colors)
+def drawCircle(x, y, size):
+    angle = 0
+    self.toggleFlowers(False)
+    while angle <= Math.PI * 2:
+        newX = x + (size * Math.cos(angle))
+        newY = y + (size * Math.sin(angle))
+        self.moveXY(newX, newY)
+        self.toggleFlowers(True)
+        angle += 0.2
+for i in range(30, 120, 10):
+    for j in range(30, 120, 10):
+        color = colors[index%len(colors)]
+        self.setFlowerColor(color)
+        index +=1
+        drawCircle(i, j, 5)
