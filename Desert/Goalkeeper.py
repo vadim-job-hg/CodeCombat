bc208b94c1a91894ba3e21abf9f164663cc0da30
vadim-job-hg <vadim-job-hg@yandex.ru>
2015-11-07 16:04:48 +0200

    Mountain

--- Desert/Goalkeeper.py
+++ Desert/Goalkeeper.py
@@ -1,1 +1,25 @@
-__author__ = 'Вадим'
+# Управляйте крестьянами, чтобы не позволить ограм забить гол.
+# Тип огненного шара (файербол) - "ball"
+def commandSoldiers(x, y):
+    index = 0
+    move = 0;
+    for soldier in self.findFriends():
+        if(index==1):
+            xx = 18
+            if x<60 and y<45:
+                yy = y + 2
+            else:
+                yy = 42
+        else:
+            xx = 18
+            if(x<30 and y>25):
+                yy = y - 2
+            else:                
+                yy = 35
+        self.command(soldier, "move", {'x':xx, 'y':yy})  
+        index +=1
+             
+loop:  
+    item = self.findNearest(self.findByType('ball'))
+    if(item):
+        commandSoldiers(item.pos.x, item.pos.y)
---
+++ DungeonsKitgard/Brawl/KithgardBrawl7.py
@@ -0,0 +1,49 @@
+def pickUpNearestItem(items):
+    nearestItem = self.findNearest(items)
+    if nearestItem:
+        moveTo(nearestItem.pos)
+        
+def moveTo(position, fast = True):    
+    if(self.isReady("jump") and self.distanceTo>10 and fast):
+        self.jumpTo(position)
+    else:
+        self.move(position)
+        
+def attack(target):
+    if target:
+        if(self.distanceTo(target)>10):
+            moveTo(enemy.pos)
+        elif(self.isReady("bash")):
+            self.bash(enemy)
+        elif(self.isReady("power-up")):
+            self.powerUp()
+            self.attack(enemy)
+        elif(self.isReady("cleave")):
+            self.cleave(enemy)
+        else:
+            self.attack(enemy)
+       
+def summonSoldier():
+    if self.gold > self.costOf('soldier'):
+        self.summon('soldier')
+
+# commands attack
+def commandSoldiers():
+    for soldier in self.findFriends():
+        enemy = self.findNearest(self.findEnemies())
+        if enemy:
+             self.command(soldier, "attack", enemy)
+loop:
+    summonSoldier()
+    commandSoldiers()
+    items = self.findItems()
+    if(len(items)>0):
+        pickUpNearestItem(items)
+    else:            
+        enemy = self.findNearest(self.findEnemies())
+        if(enemy and self.distanceTo(enemy)<10):       
+            attack(enemy)
+        elif(self.pos.x!=13 and self.pos.y!=51):
+            moveTo({'x':13, 'y':51})
+        else:
+            self.shield()
