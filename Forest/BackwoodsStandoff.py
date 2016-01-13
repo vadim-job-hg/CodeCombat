f434fe1809b0745e0dc094163e44e45c80ddd82a
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-01-14 00:55:39 +0200

    #glacier

---
+++ Forest/BackwoodsStandoff.py
@@ -0,0 +1,14 @@
+# These Munchkins are scared of the hero!
+# Say something and they'll back off.
+# However, once there are enough Munchkins, they will gang up and ambush! Be careful!
+# Whenever you can, cleave to clear the mass of enemies.
+
+loop:
+    # Use isReady to check if the hero can cleave, otherwise say something!
+    enemies = self.findEnemies();
+    enemy = self.findNearest(enemies);
+    if(enemy and self.isReady('cleave')):
+        self.cleave(enemy)
+    else:
+        self.say('DGIGURDA')
+        
