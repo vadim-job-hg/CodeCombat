e4219cdf6a827712661908afc5e8073a87850c06
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-01-16 13:11:31 +0200

    #long-kithmaze

---
+++ Forest/BackwoodsBombardier.py
@@ -0,0 +1,15 @@
+#http://codecombat.com/play/level/backwoods-bombardier
+# The pos property is an object with x and y properties.
+# pos.x is a number representing the horizontal position on the map
+# pos.y is a number representing the vertical position on the map
+
+loop:
+    enemy = self.findNearestEnemy()
+    if enemy:
+        x = enemy.pos.x
+        y = enemy.pos.y
+        # say the x and y position separated by a comma
+        self.say(x+','+y)
+    else:
+        self.say("Cease Fire!")
+
