893d09ae53dd418c12b99289700ce17aaead0b83
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-01-16 22:05:26 +0200

    #cupboards-of-kithgard

---
+++ DungeonsKitgard/CupboardsOfKithgard.py
@@ -0,0 +1,10 @@
+#https://codecombat.com/play/level/cupboards-of-kithgard
+self.moveUp()
+self.moveRight()
+self.moveRight()
+self.moveDown()
+self.moveDown()
+# Потом атакуй Шкаф ( "Cupboard" ) с использованием цикла.
+while True:
+    self.attack("Cupboard" )
+
