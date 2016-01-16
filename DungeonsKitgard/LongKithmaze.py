0535a2562326fcf026762e2000d20992ca213ea8
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-01-16 12:45:53 +0200

    #long-kithmaze

---
+++ DungeonsKitgard/LongKithmaze.py
@@ -0,0 +1,16 @@
+#http://codecombat.com/play/level/long-kithmaze
+# Это длинный лабиринт...
+self.moveRight()
+self.moveRight()
+self.moveUp()
+self.moveUp()
+
+# Теперь Вам нужно повторить сделанные шаги еще 2 раза, чтобы добраться до конца лабиринта.
+self.moveRight()
+self.moveRight()
+self.moveUp()
+self.moveUp()
+self.moveRight()
+self.moveRight()
+self.moveUp()
+self.moveUp()
