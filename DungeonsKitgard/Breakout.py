03d2f002b0005c543bde8353bb02cbeacd47e6f8
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-01-16 22:20:15 +0200

    #breakout

---
+++ DungeonsKitgard/Breakout.py
@@ -0,0 +1,8 @@
+# https://codecombat.com/play/level/breakout
+# Освободи союзника и очисть путь для побега!
+self.moveRight()
+self.attack('Weak Door')
+self.moveRight()
+self.moveDown()
+while True:
+    self.attack('Door')
