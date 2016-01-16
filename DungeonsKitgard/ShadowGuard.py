132cbf69393dca25cfd6843a33f798b6c86dafa9
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-01-16 22:30:10 +0200

    #shadow-guard

---
+++ DungeonsKitgard/ShadowGuard.py
@@ -0,0 +1,8 @@
+#https://codecombat.com/play/level/shadow-guard
+# Держись подальше от огра. Собери самоцветы.
+self.moveRight()
+self.moveUp()
+self.moveRight()
+self.moveDown()
+self.moveRight()
+
