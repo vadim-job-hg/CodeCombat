73ff34c386da7d9a016877485fe48445266773d6
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-01-16 22:32:44 +0200

    #cavern-survival

--- DungeonsKitgard/EnemyMine.py
+++ DungeonsKitgard/EnemyMine.py
@@ -1,1 +1,7 @@
 #https://codecombat.com/play/level/enemy-mine
+# Используйте параметры вместе с оператором движения, чтобы пройти дальше.
+self.moveRight(3)
+self.moveUp(1)
+self.moveRight(1)
+self.moveDown(3)
+self.moveRight(2)
