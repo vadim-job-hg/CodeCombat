59f0599ccf7791a304d0780526fb9130b77aaafb
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-01-17 16:10:42 +0200

    #types

--- functions/types.py
+++ functions/types.py
@@ -1,4 +1,7 @@
-enemy_types = []
-enemy_types.push({'name':'shaman','danger':10})
-enemy_types.push({'name':'witch','danger':8})
-enemy_types.push({'name':'brawler','danger':7})
+enemy_types = {}
+enemy_types['shaman'] = {'danger':10}
+enemy_types['warlock'] = {'danger':10}
+enemy_types['witch'] = {'danger':8}
+enemy_types['brawler'] = {'danger':7}
+enemy_types['ogre'] = {'danger':5}
+enemy_types['chieftain'] = {'danger':6}
