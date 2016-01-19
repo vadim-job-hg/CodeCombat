9e564aaba391d51c22b38255cce0ea02b044a62c
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2016-01-19 14:58:30 +0200

    Update types.py

--- functions/types.py
+++ functions/types.py
@@ -5,6 +5,6 @@ enemy_types['witch'] = {'danger':8}
 enemy_types['brawler'] = {'danger':7}
 enemy_types['ogre'] = {'danger':5}
 enemy_types['chieftain'] = {'danger':6}
-enemy_types['muchkin'] = {'danger':-1}
+enemy_types['muchkin'] = {'danger':1}
 enemy_types['yak'] = {'danger':-1}
 enemy_types['ice-yak'] = {'danger':-1}
