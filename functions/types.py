a7e7f21b12d018dd2702f6c40eb1ccc724b578db
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2016-01-19 18:45:59 +0200

    #types

--- functions/types.py
+++ functions/types.py
@@ -1,6 +1,9 @@
 enemy_types = {}
 enemy_types['shaman'] = {'danger':10}
 enemy_types['warlock'] = {'danger':10}
+enemy_types['arrow-tower'] = {'danger':10}
+enemy_types['catapult'] = {'danger':10}
+enemy_types['artillery'] = {'danger':10}
 enemy_types['witch'] = {'danger':8}
 enemy_types['brawler'] = {'danger':7}
 enemy_types['ogre'] = {'danger':5}
@@ -9,3 +12,4 @@ enemy_types['thrower'] = {'danger':3}
 enemy_types['muchkin'] = {'danger':2}
 enemy_types['yak'] = {'danger':-1}
 enemy_types['ice-yak'] = {'danger':-1}
+
