274a925cd906a7cdbb48a0d797571cbcdee44064
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-01-23 18:30:46 +0200

    #coded-orders

---
+++ Glacier/CodedOrders.py
@@ -0,0 +1,20 @@
+#http://codecombat.com/play/level/coded-orders
+# Читайте сообщение на знаке, чтобы определять, какие боевые единицы призывать, и где их размещать.
+# Загляните в руководство за инструкциями по толкованию приказов.
+summonTypes = {"a": "archer","s": "soldier","p": "peasant","g": "griffin-rider","P": "paladin","A": "artillery"}
+def summonTroops(num, coorX, coorY):
+    type = summonTypes[num]
+    if type!='artillery' and self.gold > self.costOf(type):
+        self.summon(type)
+    friends = self.findByType(summonTypes[num], self.findFriends())
+    self.command(friends[len(friends)-1], 'move', {'x':int(coorX), 'y':int(coorY)})
+
+sign = self.findByType("sign")[0]
+message = sign.message
+# Подсказка: проведите грамматический анализ знака, призовите боевые единицы и пошлите каждую единицу на нужное место.
+for index in range(0, len(message), 5):
+    type = message.substr(index, 1)
+    x = message.substr(index+1, 2)
+    y = message.substr(index+3, 2)
+    summonTroops(type, x, y)
+
