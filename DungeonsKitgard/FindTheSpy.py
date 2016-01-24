76954f6daa65fa3d19a372685cf8e7e20ea2396c
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-01-24 20:11:22 +0200

    #find-the-spy

---
+++ DungeonsKitgard/FindTheSpy.py
@@ -0,0 +1,14 @@
+#http://codecombat.com/play/level/find-the-spy?skip_protect_api=false
+soldiers = self.getCombatants()
+
+# Just killing the first soldier doesn't seem like a winning strategy...
+# Click the "Guide" button if you need a hint on telling who is on what team.
+for soldier in soldiers:
+    spy = soldier
+    if spy.team!='humans':
+        self.setTarget(spy)
+        break
+if spy and self.distanceTo(spy) > self.attackRange:
+  self.setAction("move")
+elif spy:
+  self.setAction("attack")
