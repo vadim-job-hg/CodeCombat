270a9dee2e6dfdca7f63061f87443bb0a341181f
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-01-24 18:37:22 +0200

    #multiplayer

---
+++ Multiplayer/MultiplayerTreasureGrove.py
@@ -0,0 +1,36 @@
+def pickUpNearestItem(items):
+    nearestItem = self.findNearest(items)
+    if nearestItem:
+        moveTo(nearestItem.pos)
+        
+def moveTo(position, fast = True):    
+    if(self.isReady("jump") and self.distanceTo(position)>10 and fast):
+        self.jumpTo(position)
+    else:
+        self.move(position)
+        
+def attack(target):
+    if target:
+        if(self.distanceTo(target)>10):
+            moveTo(target.pos)
+        elif(self.isReady("bash")):
+            self.bash(target)
+        elif(self.canCast('chain-lightning', target)):
+            self.cast('chain-lightning', target)
+        elif(self.isReady("attack")):
+            self.attack(target)
+        else:
+            self.shield()
+       
+loop:
+    items = self.findItems()
+    enemyattack = self.findNearest(self.findEnemies())
+    if enemyattack:
+        if(enemyattack):       
+            attack(enemyattack)
+        else:
+            self.shield()        
+    else:
+        if len(items)>0:
+            pickUpNearestItem(items)
+       
