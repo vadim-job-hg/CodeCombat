dca3886136f2f0abeeb33b9a1f3e3a044df3bcef
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-01-24 19:41:32 +0200

    #cavern-survival

--- Forest/ExtraExtrapolation.py
+++ Forest/ExtraExtrapolation.py
@@ -1,7 +1,7 @@
 #http://codecombat.com/play/level/extra-extrapolation
-shellAirTime = 3.4;  // in seconds
+shellAirTime = 3.4
 
-ogre = self.getNearestEnemy();
+ogre = self.getNearestEnemy()
 if(ogre):
-    self.attackXY(ogre.pos.x, ogre.pos.y);
+    self.attackXY(ogre.pos.x, ogre.pos.y)
 
