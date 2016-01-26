4214e055c243d5623ec1fe52bbc465d2e833d1fe
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-01-26 20:46:48 +0200

    #coin-mania

---
+++ Forest/CoinMania.py
@@ -0,0 +1,19 @@
+#http://codecombat.com/play/level/coin-mania
+# Each coin has a gold value associated with it, but we don't know how much.
+# In such cases, we use while loop.
+# Let's say I want to collect gold with total value 20.
+# Here we use while loop, as we don't know how long the loop will run.
+
+while self.gold<40:
+    self.moveRight()
+    
+self.moveLeft()    
+self.moveLeft()  
+while True:    
+    self.attackNearbyEnemy()
+
+# Now collect coins worth 40.
+# If you want to kill the ogre, be careful as you will need atleast 70 health.
+# Note: Mushrooms will decrease your health, which might even kill you.
+# Write the while loop smartly.
+# Your code here.
