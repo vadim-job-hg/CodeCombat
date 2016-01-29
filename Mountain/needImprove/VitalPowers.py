8f3debeae96a0fc60693da0eaba06f4f6fc3df84
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2016-01-29 10:49:05 +0200

    Update VitalPowers.py

--- Mountain/needImprove/VitalPowers.py
+++ Mountain/needImprove/VitalPowers.py
@@ -1,57 +1,125 @@
-# Этом уровень покажет, как определять собственные функции.
-# Код внутри функции не выполняется сразу. Он откладывается на потом.
-# Эта функция заставляет вашего героя поднять ближайшую монету.
-def pickUpNearestCoin():
-    items = self.findItems()
-    nearestCoin = self.findNearest(items)
-    if nearestCoin and self.distanceTo(nearestCoin)<30:
-        if self.isReady("jump"):
-            self.jumpTo(nearestCoin.pos)
-        else:
-            self.move(nearestCoin.pos)
+enemy_types = {}
+#enemy_types['door'] = {'danger':1000, 'focus':200}
+enemy_types['knight'] = {'danger':100, 'focus':50}
+enemy_types['ranger'] = {'danger':100, 'focus':50}
+enemy_types['trapper'] = {'danger':100, 'focus':50}
+enemy_types['samurai'] = {'danger':100, 'focus':50}
+enemy_types['sorcerer'] = {'danger':100, 'focus':50}
+enemy_types['burl'] = {'danger':10, 'focus':20}
+enemy_types['necromancer'] = {'danger':100, 'focus':50}
+enemy_types['captain'] = {'danger':100, 'focus':50}
+enemy_types['shaman'] = {'danger':10, 'focus':50}
+enemy_types['warlock'] = {'danger':10, 'focus':30}
+enemy_types['arrow-tower'] = {'danger':10, 'focus':20}
+enemy_types['catapult'] = {'danger':10, 'focus':100}
+enemy_types['artillery'] = {'danger':10, 'focus':100}
+enemy_types['witch'] = {'danger':8, 'focus':50}
+enemy_types['brawler'] = {'danger':7, 'focus':55}
+enemy_types['ogre'] = {'danger':5, 'focus':40}
+enemy_types['chieftain'] = {'danger':6, 'focus':35}
+enemy_types['thrower'] = {'danger':3, 'focus':22}
+enemy_types['fangrider'] = {'danger':4, 'focus':22}
+enemy_types['munchkin'] = {'danger':2, 'focus':15}
+enemy_types['yak'] = {'danger':-1, 'focus':0}
+enemy_types['ice-yak'] = {'danger':-1, 'focus':0}
+def findTarget():
+    danger = 0
+    enemy_return = None
+    for type in enemy_types.keys():
+        if enemy_types[type].danger>danger:
+            enemy =  self.findNearest(self.findByType(type))
+            if enemy and self.distanceTo(enemy)<enemy_types[type].focus:
+                enemy_return = enemy
+                danger = enemy_types[type].danger
+    return enemy_return
 
-# С помощью этой функции ваш герой призывает солдата.
-def summonSoldier():
-    # Заполни код здесь, что призвать солдата, если у тебя достаточно золота.
-    if self.gold > self.costOf("soldier"):
-        self.summon("soldier")
-
-
-# Эта функция приказывает вашим солдатам атаковать ближайшего врага.
-def commandSoldiers():
-    for soldier in self.findFriends():
-        enemy = soldier.findNearestEnemy()
-        if enemy:
-            self.command(soldier, "attack", enemy)
-
+def pickUpNearestItem(items):
+    nearestItem = self.findNearest(items)
+    if nearestItem:
+        moveTo(nearestItem.pos)
+        
+def moveTo(position, fast = True):    
+    if(self.isReady("jump") and self.distanceTo(position)>10 and fast):
+        self.jumpTo(position)
+    else:
+        self.move(position)
+        
 def attack(target):
     if target:
-        if(self.isReady("jump") and self.distanceTo>10):
-            self.jumpTo(enemy.pos)
+        if(self.distanceTo(target)>10):
+            moveTo(target.pos)
         elif(self.isReady("bash")):
-            self.bash(enemy)
-        elif(self.isReady("power-up")):
-            self.powerUp()
-            self.attack(enemy)
-        elif(self.isReady("cleave")):
-            self.cleave(enemy)
+            self.bash(target)
+        elif(self.canCast('chain-lightning', target)):
+            self.cast('chain-lightning', target)
+        elif(self.isReady("attack")):
+            self.attack(target)
         else:
-            self.attack(enemy)
+            self.shield()
+       
+summonTypes = ['soldier','soldier','soldier','soldier','soldier','soldier','soldier','soldier','soldier','soldier','soldier','soldier','soldier','soldier','paladin']
+def summonTroops():
+    type = summonTypes[len(self.built)%len(summonTypes)]
+    if self.gold > self.costOf(type):
+        self.summon(type)
+        
+def сommandTroops():
+    for index, friend in enumerate(self.findFriends()):
+        if friend.type == 'archer':
+            CommandArcher(friend)        
+        elif friend.type == 'paladin':
+            CommandPaladin(friend)   
+        elif friend.type == 'soldier':
+            CommandSoldier(friend)
+        elif friend.type == 'peasant':
+            CommandPeasant(friend)
+            
+def CommandPaladin(paladin):
+    if(paladin.canCast ("heal")):
+        if(self.health<self.maxHealth*0.6):
+            target = self
+        else:
+            target = lowestHealthFriend()        
+        if target:
+            self.command(paladin, "cast", "heal", target)
+    elif enemyattack:
+            self.command(paladin, "attack", enemyattack)
 
-def tacktick():
-    enemies = self.findEnemies()
-    nearest = self.findNearest(enemies)
+def CommandSoldier(soldier):
+    if enemyattack:
+        self.command(soldier, "attack", enemyattack)
+
+def CommandArcher(soldier):
+    if enemyattack:
+        self.command(soldier, "attack", enemyattack)
+
+def CommandPeasant(soldier):
+    if enemyattack:
+        self.command(soldier, "attack", enemyattack)
+
+def lowestHealthFriend():
+    lowestHealth = 99999
+    lowestFriend = None
     friends = self.findFriends()
-    if self.distanceTo(nearest)<10:
-        attack(nearest)
-    elif len(friends)/3<len(enemies):
-        attack(nearest)
-    else:
-        pickUpNearestCoin()
-
+    for friend in friends:
+        if friend.health < lowestHealth and friend.health < friend.maxHealth:
+            lowestHealth = friend.health
+            lowestFriend = friend
+    return lowestFriend
 loop:
-    # В своем цикле ты можешь "вызывать" функции, определенные выше.
-    # Эта строка вызывает выполнение кода внутри функции "pickUpNearestCoin" .
-    pickUpNearestCoin()
-    summonSoldier()  
-    commandSoldiers()
+    enemyattack = findTarget()
+    if not enemyattack:
+        enemyattack = self.findNearest(self.findEnemies())
+    summonTroops()
+    сommandTroops()
+    items = self.findItems()
+    enimies = self.findEnemies()
+    #for enemy in enimies:
+    #    self.say(enemy.type)
+    if(len(items)>0 and self.now()<35):
+        pickUpNearestItem(items)
+    else: 
+        if(enemyattack and enemyattack.type!='munchkin' and enemyattack.type!='thrower' and enemyattack.type!='scout' and self.now()<45):     
+            attack(enemyattack)
+        else:
+            self.shield()
