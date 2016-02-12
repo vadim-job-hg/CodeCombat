5c78b2ff9927ebe1a7c2f57a6e975ce61e89e9b5
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-02-12 20:35:13 +0200

    #orge-rush

---
+++ Auditions/OrgeRush.py
@@ -0,0 +1,26 @@
+#http://codecombat.com/play/level/orge-rush
+# Should fill in some default source
+def moveTo(position, fast = True):
+    if(self.isReady("jump") and self.distanceTo(position)>10 and fast):
+        self.jumpTo(position)
+    else:
+        self.move(position)
+
+def attack(target):
+    if target:
+        if(self.distanceTo(target)>10):
+            moveTo(target.pos)
+        elif(self.isReady("bash")):
+            self.bash(target)
+        elif(self.canCast('chain-lightning', target)):
+            self.cast('chain-lightning', target)
+        elif(self.isReady("attack")):
+            self.attack(target)
+        else:
+            self.shield()
+
+loop:
+    enemy = self.findNearest(self.findEnemies())
+    if enemy:
+        attack(enemy)
+
