4bf19aba90c06bc794ff672ef761d14820d0769a
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-02-13 21:53:10 +0200

    #auditions

---
+++ Multiplayer/DungeonArenaO.js
@@ -0,0 +1,15 @@
+#http://codecombat.com/play/ladder/dungeon-arena#ogres
+this.attacksomeone = function (e, type) {
+    if (!this.getCooldown('stomp') && type.indexOf('stomp') != -1) {
+        this.stomp();
+    } else if (!this.getCooldown('throw') && type.indexOf('throw') != -1) {
+        this.throw(e);
+    } else {
+        this.attack(e);
+    }
+};
+var nows = this.now();
+var enemies = this.getEnemies();
+if(enemies[1]){
+    this.attacksomeone(enemies[0], ['stomp', 'throw']);
+}
