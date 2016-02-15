f742a469d742bfd9b6f1f2e2e4a51e7279e40af6
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2016-02-16 00:12:19 +0200

    Create EnemMine.py

---
+++ DungeonsKitgard/EnemMine.py
@@ -0,0 +1,7 @@
+#http://codecombat.com/play/level/enemy-mine
+# Используйте параметры вместе с оператором движения, чтобы пройти дальше.
+self.moveRight(3)
+self.moveUp(1)
+self.moveRight(1)
+self.moveDown(3)
+self.moveRight(2)
