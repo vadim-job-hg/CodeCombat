24c7112d44e664ba2883bc653ddf4a40d7bdee23
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2016-02-18 18:46:54 +0200

    #serpent-savings

--- Glacier/SerpentSavings.py
+++ Glacier/SerpentSavings.py
@@ -1,4 +1,4 @@
-
+# http://codecombat.com/play/level/serpent-savings
 # You cannot collect coins.
 # Summon peasants to collect coins for you.
 # Collecting coins spawns a growing 'tail' behind the peasants.
