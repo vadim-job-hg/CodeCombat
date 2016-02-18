cc07d634ba4fddd135b57fc487bb33496c397609
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2016-02-18 18:14:41 +0200

    Update SerpentSavings.py

--- Glacier/SerpentSavings.py
+++ Glacier/SerpentSavings.py
@@ -1,1 +1,19 @@
 
+# You cannot collect coins.
+# Summon peasants to collect coins for you.
+# Collecting coins spawns a growing 'tail' behind the peasants.
+# When a peasant touches a tail, they die.
+# Collect 500 coins to pass the level.
+# The following APIs are available on your team's peasants: "snakeBackward"
+# The following APIs are available on neutral peasants: "snakeBackward", "snakeHead", "snakeForward"
+
+
+self.summon("peasant");
+loop:
+    friends = self.findFriends()
+    tails = self.findEnemies()
+    coins = self.findItems()
+    for friend in friends:
+        # Command the peasant to collect a coin, while avoiding the tails.
+        
+        pass
