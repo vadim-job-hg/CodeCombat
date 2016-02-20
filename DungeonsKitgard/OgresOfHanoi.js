f65234cd13a578544bf26084997f01f5f313200e
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-02-20 10:25:31 +0200

    #old-games

---
+++ DungeonsKitgard/OgresOfHanoi.js
@@ -0,0 +1,14 @@
+//http://codecombat.com/play/level/ogres-of-hanoi
+var that = this;
+ 
+function hanoi(n, from, to, via) {
+if (n === 1) {
+that.say("move", [from, to]);
+} else {
+hanoi(n-1, from, via, to);
+that.say("move", [from, to]);
+hanoi(n-1, via, to, from);
+}
+}
+ 
+hanoi(5, 2, 3, 1);
