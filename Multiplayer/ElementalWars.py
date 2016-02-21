2cf430a0496519fcb9be2a938a97c2cf4f9f2af1
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-02-21 15:54:31 +0200

    #new-games

---
+++ Multiplayer/ElementalWars.py
@@ -0,0 +1,52 @@
+#elemental-wars
+#http://codecombat.com/play/ladder/ace-of-coders
+# Победите героя противника менее, чем за три минуты.
+
+def buildArmy():
+    # Ваш герой может вызывать дружественные отряды и управлять ими.
+
+    buildOrder = ["fire-soldier","fire-soldier","fire-soldier","fire-soldier","fire-soldier","fire-soldier","fire-soldier","fire-soldier","fire-soldier","fire-soldier", "fire-archer","fire-soldier", "fire-archer", "fire-soldier", "fire-archer"]
+    #"arrow-tower" "artillery"
+    type = buildOrder[len(self.built) % len(buildOrder)]
+    if self.gold >= self.costOf(type):
+        self.summon(type)
+
+def commandArmy():
+    friends = self.built
+    enemies = self.findEnemies()
+    points = self.getControlPoints()
+    for i, friend in enumerate(friends):
+        if friend.health <= 0 or friend.type == "arrow-tower":
+            continue
+        # Управляйте своей армией, чтобы захватить ключевые точки.
+        # Убедитесь, что ключевые точки выбраны с умом!
+
+        point = points[i % len(points)]
+        if self.now() < 30:
+            self.command(friend, "defend", point.pos)
+        else:
+            enemies = self.findEnemies()
+            nearestEnemy = self.findNearest(enemies)
+            self.command(friend, "attack", nearestEnemy)
+def attack(target):
+    if target:
+        if self.isReady("throw") and self.distanceTo(target)<self.throwRange:
+            self.throw(target)
+        elif(self.distanceTo(target)>10):
+            self.move(target.pos)
+        elif(self.isReady("stomp") and self.distanceTo(target)<5):
+            self.stomp()
+        elif(self.isReady("attack")):
+            self.attack(target)
+
+def controlHero():
+    enemies = self.findEnemies()
+    nearestEnemy = self.findNearest(enemies)
+    shouldAttack = self.now() > 30
+    # Используйте возможности героя, чтобы переломить ситуацию в свою пользу.
+    if shouldAttack:
+        attack(nearestEnemy)
+loop:
+    buildArmy()
+    commandArmy()
+    controlHero()
