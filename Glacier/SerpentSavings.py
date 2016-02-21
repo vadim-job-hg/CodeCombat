ff67bb1c289bd1e1517e4815090ceb254afe9a52
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-02-21 15:18:41 +0200

    #new-games

--- Glacier/SerpentSavings.py
+++ Glacier/SerpentSavings.py
@@ -19,16 +19,27 @@ def pickUpNearestItem(items):
         moveTo(nearestItem.pos)
         
 def commandPeasant(peasant):
-   item = peasant.findNearestItem()
-   if item:
-       self.command(peasant, 'move', item.pos)
+    item = peasant.findNearestItem()
+    goalf = item.pos
+    #if item:
+    #    goalf = Vector.subtract(goalf, item.pos)
+    #    goalf = Vector.normalize(goalf)
+    #    goalf = Vector.multiply(goalf, 10)
+    enemies = peasant.findEnemies()
+    #for enemy in enemies:
+    #    if peasant.distanceTo(enemy)<10:
+    #        vectorToH = Vector.subtract(friend.pos, enemy.pos)
+    #        vectorToH = Vector.normalize(vectorToH)
+    #        vectorToH = Vector.multiply(vectorToH, 10)
+    #       goalf =  Vector.add(vectorToH, goalf)            
+    self.command(peasant, 'move', goalf)
        
 def CommandArcher(soldier):
     target = self.findNearest(self.findEnemies())
     if target:
         self.command(soldier, "attack", target)       
         
-summonTypes = ['peasant', 'archer','peasant', 'archer','peasant', 'archer','peasant','peasant','peasant','peasant', 'archer', 'archer','peasant','peasant']
+summonTypes = ['peasant', 'archer', 'archer', 'archer','archer']
 def summonTroops():
     type = summonTypes[len(self.built)%len(summonTypes)]
     if self.gold > self.costOf(type):
@@ -36,7 +47,7 @@ def summonTroops():
 
 loop:    
     friends = self.findFriends()
-    summonTroops()
+    #summonTroops()
     tails = self.findEnemies()
     coins = self.findItems()
     #pickUpNearestItem(coins)
