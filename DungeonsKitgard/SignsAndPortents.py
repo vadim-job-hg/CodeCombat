3ab67015e564ca045ae22c7b43970c733cec0a65
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2016-03-01 20:48:27 +0200

    Create SignsAndPortents.py

---
+++ DungeonsKitgard/SignsAndPortents.py
@@ -0,0 +1,14 @@
+# Ваша цель добраться до правой части карты живым. Ваши союзники помогут вам.
+# You don't need to fight the ogres, just move! Your allies will protect you.
+#http://codecombat.com/play/level/signs-and-portents
+self.moveRight()
+self.moveRight()
+self.moveUp()
+self.moveRight()
+self.moveRight()
+self.moveRight()
+self.moveDown()
+self.moveRight()
+self.moveDown()
+self.moveRight()
+ 
