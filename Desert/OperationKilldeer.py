7d5111706c1127d7d826062d5539457818510c7e
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-04-07 20:03:54 +0300

    #operation-killdeer

---
+++ Desert/OperationKilldeer.py
@@ -0,0 +1,18 @@
+#http://codecombat.com/play/level/operation-killdeer
+# Lure the ogres into a trap.
+# But, they are careful.
+# These ogres will only follow the hero if the hero is injured.
+
+# This function checks the hero's health and returns a Boolean value.
+def shouldRun():
+    if self.health < self.maxHealth / 2:
+        return True
+    else:
+        return False
+
+while True:
+    # Run to the X only if the hero shouldRun().
+    if(shouldRun()):
+        self.moveXY(75, 37)
+    # Otherwise, fight!
+
