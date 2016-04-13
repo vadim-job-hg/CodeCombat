e5cf32d58a5cf36414958115b9e5c357b712bb50
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2016-04-13 21:35:36 +0300

    Update LeaveItToCleaver.py

--- Forest/LeaveItToCleaver.py
+++ Forest/LeaveItToCleaver.py
@@ -1,1 +1,22 @@
+#http://codecombat.com/play/level/leave-it-to-cleaver
+# This shows how to define a function called cleaveWhenClose
+# The function accepts defines a parameter called target
+def cleaveWhenClose(target):
+    if hero.distanceTo(target) < 5:
+        pass
+        # Put your attack code here
+        # If cleave is ready, then cleave target
+        if(self.isReady('cleave')):
+            self.cleave(target);
+        # else, just attack target!
+        else:
+            self.attack(target)
+
+# This code is not part of the function.
+while True:
+    enemy = hero.findNearestEnemy()
+    if enemy:
+        # Note that inside cleaveWhenClose, we refer to the enemy as target.
+        cleaveWhenClose(enemy)
+
 
