44966adc17566c56e0cad782d9f0fbf394f34647
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2016-04-18 20:45:03 +0300

    Update TheWizardsDoor.py

--- Forest/TheWizardsDoor.py
+++ Forest/TheWizardsDoor.py
@@ -1,1 +1,22 @@
 #https://codecombat.com/play/level/the-wizards-door
+# Move to Laszlo and get the secret number from him.
+self.moveXY(30, 13)
+secret = self.findNearestFriend().getSecret()
+secretr = secret
+number = secret + 7
+# Follow Laszlo's instructions to get the magic number!
+# Move to Erzsebet and say her magic number.
+self.moveXY(17, 26)
+self.say(number)
+secret = self.findNearestFriend().getSecret()
+number = (7 + secret)%4
+# Move to Simonyi and say his magic number.
+self.moveXY(30, 39)
+self.say(number)
+secret = self.findNearestFriend().getSecret()
+number = (secret)*secretr
+# Move to Agata and say her magic number.
+self.moveXY(43, 26)
+self.say(0)
+#self.say(number)
+
