a823296c31502a396e70c8e5da60a46dab3dc5b2
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-04-21 22:00:51 +0300

    #brittle-morale

---
+++ Desert/BrittleMorale.py
@@ -0,0 +1,21 @@
+#http://codecombat.com/play/level/brittle-morale
+# Ogres are strong, but cowardly.
+# Find and kill leader and they will retreat.
+# You have only have one shot, but a deadly shot it will be.
+
+# This function should return the enemy with the most health.
+def findStrongestEnemy(enemies):
+    strongest = None
+    strongestHealth = 0
+    enemyIndex = 0
+    # Iterate over all the ogres to find the one with the most health.
+    enemies = hero.findEnemies()
+    for enemy in enemies:
+        if enemy.health>strongestHealth:
+            strongestHealth = enemy.health
+            strongest = enemy
+    return strongest
+
+enemies = self.findEnemies()
+if enemies:
+    self.say(findStrongestEnemy(enemies))
