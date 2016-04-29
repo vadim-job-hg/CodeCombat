54246d384ff932e73693d9cd342e61dd33c6f368
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-04-30 00:30:54 +0300

    #salted-earth

---
+++ Forest/SaltedEarth.py
@@ -0,0 +1,16 @@
+#http://codecombat.com/play/level/salted-earth
+# Ogres are attacking a nearby settlement!
+# Be careful, though, for the ogres have sown the ground with poison.
+# Gather coins and defeat the ogres, but avoid the burls and poison!
+
+while True:
+    enemy = hero.findNearestEnemy()
+    if enemy.type == "munchkin" or enemy.type == "thrower":
+        hero.attack(enemy)
+    #item = hero.findNearestItem()
+    item = hero.findNearest(hero.findItems())
+    if(item.type=='gem' or item.type=='coin'):
+        hero.move(item.pos)
+    # Check the item type to make sure the hero doesn't pick up poison!
+    # Look for types: 'gem' and 'coin'
+
