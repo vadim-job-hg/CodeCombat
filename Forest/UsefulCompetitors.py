5cc3bfd3dc340ae609cf5fc6cd6cdf41d9c32cd7
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-04-30 00:36:58 +0300

    #useful-competitors

--- Forest/UsefulCompetitors.py
+++ Forest/UsefulCompetitors.py
@@ -1,1 +1,16 @@
 #http://codecombat.com/play/level/useful-competitors
+# The coin field has been seeded with vials of deadly poison.
+# Ogres are attacking, while their peons are trying to steal your coins!
+# Let the peons pick up the poison, and only gather the coins and gems.
+
+while True:
+    enemy = hero.findNearestEnemy()
+    if enemy:
+        if not enemy.type is "peon":
+            hero.attack(enemy)
+    #item = hero.findNearestItem()
+    item = hero.findNearest(hero.findItems())
+    if item and item.type!='poison':
+        hero.move(item.pos)
+        # Gather the item only if it is not poison.
+
