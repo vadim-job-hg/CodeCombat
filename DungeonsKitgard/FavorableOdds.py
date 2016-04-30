6d1826755ef6581f7e8a0f87b002c6c82c34f059
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-04-30 17:44:04 +0300

    #favorable-odds

---
+++ DungeonsKitgard/FavorableOdds.py
@@ -0,0 +1,11 @@
+#http://codecombat.com/play/level/favorable-odds
+# Атакуй обоих огров и хватай самоцветы.
+hero.moveRight()
+hero.attack('Krug')
+hero.attack('Krug')
+hero.moveRight()
+hero.moveUp()
+hero.attack('Grump')
+hero.attack('Grump')
+hero.moveLeft()
+hero.moveLeft()
