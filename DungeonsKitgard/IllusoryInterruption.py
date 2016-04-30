588c2d46e2f72667f33260094c1e0f2c3ee78bfb
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-04-30 18:23:58 +0300

    #illusory-interruption

---
+++ DungeonsKitgard/IllusoryInterruption.py
@@ -0,0 +1,6 @@
+#http://codecombat.com/play/level/illusory-interruption
+# Используй приманку чтобы отвлечь стражу наступая на Х.
+hero.moveRight()
+hero.moveDown(2)
+hero.moveUp(2)
+hero.moveRight(5)
