2492b7d1b48a109ff5208c621f72581150328765
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-04-30 18:42:50 +0300

    #the-skeleton

---
+++ DungeonsKitgard/HackAndDash.py
@@ -0,0 +1,15 @@
+#http://codecombat.com/play/level/hack-and-dash
+# Вы можете написать код перед петлей ('loop')
+# Сломайте "Сундук" перед использование команды loop чтобы покинуть лабиринт!
+hero.moveRight()
+hero.moveUp()
+hero.attack('Chest')
+hero.moveDown()
+while True:
+    # Сделайте 3 хода.
+    hero.moveRight(3)
+    hero.moveDown(3)
+
+
+
+
