04166f4ae3e3e6bd82a9d6b64518c462faac745e
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-05-01 11:38:48 +0300

    #robot-ragnarok

---
+++ DungeonsKitgard/RobotRagnarok.py
@@ -0,0 +1,14 @@
+#http://codecombat.com/play/level/robot-ragnarok
+# The robot ragnarok is upon us!
+# Dodge the incoming plasma balls for 15 seconds.
+loop:
+    hero.moveUp()
+    hero.moveUp()
+    hero.moveDown()
+    hero.moveDown()
+    hero.moveLeft()
+    hero.moveRight()
+    hero.moveDown()
+    hero.moveRight()
+    hero.moveRight()
+    break
--- Glacier/BombingRun.py
+++ Glacier/BombingRun.py
@@ -1,16 +1,19 @@
 # http://codecombat.com/play/level/bombing-run
-
 # Incoming oscars! (That's military speak for ogres).
-# You will need to calculate their angle of attack.
-# Use that angle to command your Griffin Bombers!
+# Тебе нужно рассчитать угол их атаки.
+# Используй тригонометрию, чтобы найти угол в радианах!
 
 while True:
-    enemy = self.findNearestEnemy();
-    if enemy:
-         # Find the vector of attack
-        #diff = enemy.pos.subtract(self.pos)
-        vector = Math.atan2(enemy.pos.y, enemy.pos.x)
-        vector = vector * 180 / Math.PI+30
-        # Say the angle!
-        self.say(vector)
-        
+    enemy = hero.findNearest(hero.findEnemies())
+    if enemy and hero.distanceTo(enemy)<200:
+        O=Math.abs(enemy.pos.y -self.pos.y)
+        A=Math.abs(enemy.pos.x-self.pos.x)
+        angle=Math.atan2(O,A)
+        angle=angle*180/Math.PI
+        if enemy.pos.x<self.pos.x:
+            angle=180-angle
+        hero.say(angle)
+        hero.wait(3)
+
+
+
