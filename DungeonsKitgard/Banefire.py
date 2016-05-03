aa2bec211dac85424c7b1e489450e95035559820
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-05-03 19:43:38 +0300

    #banefire

---
+++ DungeonsKitgard/Banefire.py
@@ -0,0 +1,11 @@
+#http://codecombat.com/play/level/banefire
+# Огр выглядит большим и медленным. Передвижение поможет тебе избежать его смертельных атак.
+hero.moveRight()
+hero.moveUp()
+hero.moveDown()
+hero.moveUp()
+hero.moveDown()
+hero.moveRight()
+loop:
+    hero.moveUp()
+    hero.moveDown()
