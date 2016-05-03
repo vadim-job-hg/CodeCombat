797d8a320b2ee000e0e8c9ec2fda8b0455416dd1
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-05-03 19:36:47 +0300

    #attack-wisely

---
+++ DungeonsKitgard/AttackWisely.py
@@ -0,0 +1,79 @@
+#http://codecombat.com/play/level/attack-wisely
+# Избегите всех огненных ловушек
+# Если вы ступите на огненную ловушку, то Вы будете взорваны
+hero.moveUp();
+hero.moveRight();
+#hero.moveUp(); # Удалите эту линию
+hero.moveRight();
+hero.moveUp();
+hero.moveRight(5);
+enemy = self.findNearestEnemy()
+while enemy:
+    self.attack(enemy)
+    enemy = self.findNearestEnemy()
+hero.moveUp(3);
+hero.moveLeft();
+hero.moveDown(3);
+hero.moveLeft(2);
+enemy = self.findNearestEnemy()
+while enemy:
+    self.attack(enemy)
+    enemy = self.findNearestEnemy()
+hero.moveUp(2);
+enemy = self.findNearestEnemy()
+while enemy:
+    self.attack(enemy)
+    enemy = self.findNearestEnemy()
+hero.moveDown(2);
+hero.moveLeft(2);
+enemy = self.findNearestEnemy()
+while enemy:
+    self.attack(enemy)
+    enemy = self.findNearestEnemy()
+hero.moveUp(2);
+enemy = self.findNearestEnemy()
+while enemy:
+    self.attack(enemy)
+    enemy = self.findNearestEnemy()
+hero.moveDown(3);
+hero.moveLeft(2);
+hero.moveUp();
+enemy = self.findNearestEnemy()
+while enemy:
+    self.attack(enemy)
+    enemy = self.findNearestEnemy()
+hero.moveUp(2);
+enemy = self.findNearestEnemy()
+while enemy:
+    self.attack(enemy)
+    enemy = self.findNearestEnemy()
+hero.moveDown(3);
+hero.moveRight(2);
+hero.moveDown();
+# Подсказка 1: Чем больше враг, тем он сильнее
+# Подсказка 2: Ты не должен убивать всех огров, если чувствуешь что недостаточно силен!
+
+
+# Двигайтесь ближе к двери
+
+
+# Атакуйте дверь при помощи команды "this.attack" ('DoorName')
+
+
+# Заходите в комнату
+
+
+# Attack the enemy inside the room (How to attack a no-named enemy ?)
+
+
+# Move to gem alcove
+
+
+# Атакуйте врагов
+
+
+# Взять алмазы
+
+
+# Now let's escape (move to the x-mark)
+# Снова, избегите всех огненных ловушек
