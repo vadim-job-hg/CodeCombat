f68d63edb0641c7ea554cdd8c3dd66ff3372deb4
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-05-03 20:14:39 +0300

    #crawlways-of-kithgard

--- DungeonsKitgard/CrawlwaysOfKithgard.py
+++ DungeonsKitgard/CrawlwaysOfKithgard.py
@@ -1,4 +1,4 @@
-http://codecombat.com/play/level/crawlways-of-kithgard
+#http://codecombat.com/play/level/crawlways-of-kithgard
 # Не попадайтесь на глаза ограм.
 self.wait(5)
 self.moveRight(2)
---
+++ DungeonsKitgard/ThePrisoner.py
@@ -0,0 +1,12 @@
+#http://codecombat.com/play/level/the-prisoner
+# Освободите Патрика из-за "Weak Door".
+self.moveRight()
+self.attack('Weak Door')
+# Убейте охранника, с именем "Two".
+self.moveRight(3)
+# Собирайте самоцветы.
+self.moveDown(3)
+self.attack('Two')
+self.attack('Two')
+self.attack('Two')
+self.attack('Two')
