fddcb685c7de591deb878714783291e309a12b3f
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-05-05 23:10:04 +0300

    #glacier

---
+++ Mountain/ResourceValleys.py
@@ -0,0 +1,37 @@
+#http://codecombat.com/play/level/resource-valleys
+# Collect all the coins!
+# The peasants are unable to get the coins from other areas
+# However, each area only spawns a certain value of coin!
+# Filter through all the items and command the peasants accordingly.
+
+def commandPeasant(peasant, coins):
+    # Command the peasant to find the nearest of their coins array:
+    item = peasant.findNearest(coins)
+    if item:
+        self.command(peasant, "move", item.pos)
+
+friends = hero.findFriends()
+peasants = {
+    "Aurum": friends[0],
+    "Argentum": friends[1],
+    "Cuprum": friends[2]
+}
+
+while True:
+    items = hero.findItems()
+    goldCoins = []
+    silverCoins = []
+    bronzeCoins = []
+    for i in range(len(items)):
+        item = items[i]
+        if item.value == 3:
+            goldCoins.push(item)
+        # Put bronze and silver coins in their approriate array:
+        elif item.value == 2:
+            silverCoins.push(item)
+        else:
+            bronzeCoins.push(item)
+
+    commandPeasant(peasants.Aurum, goldCoins)
+    commandPeasant(peasants.Argentum, silverCoins)
+    commandPeasant(peasants.Cuprum, bronzeCoins)
