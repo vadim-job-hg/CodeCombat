f84e78449fdf78426f7151f386853a39a440453f
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-05-05 21:23:56 +0300

    #glacier

---
+++ Forest/BurlboleGrove.py
@@ -0,0 +1,22 @@
+# http://codecombat.com/play/level/burlbole-grove
+# Don't attack the burls!
+# Functions can return a value.
+# When a function is called, it will be equal to the value the function returns.
+
+def shouldAttack(target):
+	# return False if no target
+    if not target:
+        return False
+	# return False if target.type == "burl"
+    if target.type=="burl":
+        return False
+    # Otherwise, return True
+    return True
+
+while True:
+    enemy = hero.findNearestEnemy()
+    # Here we use shouldAttack() to decide if we should attack!
+    # heroShouldAttack will be assigned the same value that shouldAttack() returns!
+    heroShouldAttack = shouldAttack(enemy)
+    if heroShouldAttack:
+    	hero.attack(enemy)
