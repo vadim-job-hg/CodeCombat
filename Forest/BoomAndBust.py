5816005ba1383b4e6a3c40551a97da5e4e1b50b9
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-05-09 11:02:21 +0300

    #glacier

---
+++ Forest/BoomAndBust.py
@@ -0,0 +1,8 @@
+#http://codecombat.com/play/level/boom-and-bust
+# Используй функцию buildXY чтобы построить две "fire-traps" возле двери.
+# Они детонируют когда ты отбежить на безопасную дистанцию.
+# Убегай в лес!
+self.buildXY('fire-trap', 35, 35)
+self.buildXY('fire-trap', 35, 30)
+self.moveXY(11, 30)
+self.moveXY(73, 30)
