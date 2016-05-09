61e4b130feca6e8c006c9a3004a4578a356e9bec
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-05-09 10:56:42 +0300

    #glacier

---
+++ Forest/DeadlyPursuit.py
@@ -0,0 +1,15 @@
+#http://codecombat.com/play/level/deadly-pursuit
+# Собери все монеты и используй флаги для установки ловушек за собой.
+# Разберись с этими ограми.
+
+while True:
+    flag = hero.findFlag()
+    #item = hero.findNearestItem()
+    item = hero.findNearest(hero.findItems())
+    if flag:
+        hero.buildXY('fire-trap', flag.pos.x, flag.pos.y)
+        hero.pickUpFlag(flag)
+    elif item:
+        hero.moveXY(item.pos.x, item.pos.y)
+
+
