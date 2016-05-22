c8e0f0b4e52c51f1edbc5bcfe5907cba5152495c
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-05-22 12:46:11 +0300

    #before-video

--- Forest/TheHuntBegins.py
+++ Forest/TheHuntBegins.py
@@ -1,2 +1,27 @@
 #http://codecombat.com/play/level/the-hunt-begins
-#todo:
+# Senick is trying to find the elusive Burleous Majoris!
+# But he doesn't know how big a Burleous Majoris would be...
+# Find the average size of this burl population to use as a baseline!
+
+# This function returns average size of all the burls in an array.
+def averageSize(burls):
+    sum = sumSize(burls)
+    # Remember the average is the sum of the parts divided by the amount!
+    return sum / burls.length
+
+# This function should return the sum of all the burls sizes.
+def sumSize(burls):
+    sum = 0
+    for burl in burls:
+        sum = sum + burl.size
+    return sum
+    # Implement the sum function using the burls 'size':
+
+
+while True:
+    # Find the average size of the burls by calling the 'averageSize' function.
+    enemies =hero.findEnemies()
+    # Say the average size of the seen burls!
+    hero.say(averageSize(enemies))
+
+
