bdad60499e1bc78f6af135dc0085d18bcaee5a98
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-05-26 22:28:46 +0300

    fair-battle

---
+++ Desert/WishingWell.py
@@ -0,0 +1,36 @@
+#http://codecombat.com/play/level/wishing-well
+# You need exactly 104 gold.
+# Calculate how much gold is on the field, and say the phrases to increase or decrease the amount.
+
+less = "Nimis"
+more = "Non satis"
+requiredGold = 104
+
+# This function calculates the sum of coin values.
+def sumCoinValues(coins):
+    i = 0
+    total = 0
+    while i < len(coins):
+        total += coins[i].value
+        i += 1
+    return total
+
+while True:
+    items = hero.findItems()
+    goldAmount = sumCoinValues(items)
+    if len(items) != 0:
+        # If there is not enough gold, then say "Non satis".
+        if goldAmount>104:
+        # If there is too much gold, then say "Nimis".
+            self.say(less)
+        # If there is exactly 104 gold, then collect all coins.
+        if goldAmount<104:
+            self.say(more)
+        pass
+        if goldAmount==104:
+            break
+while True:
+    items = hero.findItems()
+    item = hero.findNearest(items)
+    if item:
+        hero.move(item.pos)
