77ec334dd1c44fab084f9d405521bde28a1abb37
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-05-26 22:22:12 +0300

    fair-battle

--- Desert/FairBattle.py
+++ Desert/FairBattle.py
@@ -1,1 +1,22 @@
 #http://codecombat.com/play/level/fair-battle
+# Today is the day for a fair battle!
+# Wait until the total health of your soldiers is greater than ogre's.
+# Don't attack while your soldiers don't have the advantage.
+
+# Write a function to return the sum of health of an array:
+def Calc(friends, enemies):
+    frh = 0
+    enh = 0
+    for friend in friends:
+        frh = frh + friend.health
+    for enemy in enemies:
+        enh = enh + enemy.health
+    return frh>enh
+while True:
+    friends = hero.findFriends()
+    enemies = hero.findEnemies()
+    # Calculate and compare the total health of your soldiers and the ogres.
+    if Calc(friends, enemies):
+        self.say("Attack")
+    # Say "Attack" when you are ready.
+
