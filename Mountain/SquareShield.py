14d577671c2f599ab8ce3bc5d8a903d867284575
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-05-26 22:48:13 +0300

    square-shield

--- Mountain/SquareShield.py
+++ Mountain/SquareShield.py
@@ -1,1 +1,25 @@
 #http://codecombat.com/play/level/square-shield
+# Incoming yeti attack!
+# Use your paladins to form a square!
+# Command Illumina and Vaelia to move to the corners of the square.
+
+def findByName(name, thangs):
+    for i in range(len(thangs)):
+        thang = thangs[i]
+        if thang.id == name:
+            return thang
+    return None
+
+friends = hero.findFriends()
+celadia = findByName("Celadia", friends)
+dedalia = findByName("Dedalia", friends)
+
+sideLength = celadia.distanceTo(dedalia)
+
+# First find the remaining paladins and assign them to variables:
+illumina = findByName("Illumina", friends)
+vaelia = findByName("Vaelia", friends)
+# Command both to move to the corners of the square.
+# Remember squares have equal-length sides!
+self.command(illumina, 'move',{'x':28,'y':22})
+self.command(vaelia, 'move',{'x':54,'y':22})
