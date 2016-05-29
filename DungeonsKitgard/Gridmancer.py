6a4c62006b9fb9976064014ae9c481e6a35b273b
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-05-29 15:55:17 +0300

    #before-video

--- DungeonsKitgard/Gridmancer.py
+++ DungeonsKitgard/Gridmancer.py
@@ -1,2 +1,5 @@
-#http://codecombat.com/play/level/gridmancer?session=56c5ebfded946a44004fb659&observing=true
+#http://codecombat.com/play/level/gridmancer
+
+# ?session=56c5ebfded946a44004fb659&observing=true
+?session=562b6ecbc953814916b52d20&observing=true
 #todo:
