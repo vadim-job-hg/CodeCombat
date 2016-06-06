d172127c383e88c67d859fd9d0a2a94bc5933059
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2016-06-06 21:05:57 +0300

    Update BackwoodsBuddy.py

--- Forest/BackwoodsBuddy.py
+++ Forest/BackwoodsBuddy.py
@@ -1,1 +1,14 @@
 #https://codecombat.com/play/level/backwoods-buddy
+# You now have a pet!
+
+def speak(event):
+    pet.say("Hi")
+    pass
+    # Your pet should respond with pet.say()
+    
+
+# This tells your pet to run the speak() function when she hears something
+pet.on("hear", speak)
+
+# Talk to your pet!
+hero.say("Hello Kitty")
