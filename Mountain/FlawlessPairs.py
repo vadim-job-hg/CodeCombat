71c798b79297dcb3103ee87448560c8e2bc0404c
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-06-08 21:30:08 +0300

    #before-video

---
+++ Mountain/FlawlessPairs.py
@@ -0,0 +1,32 @@
+#https://codecombat.com/play/level/flawless-pairs
+# Collect 4 pairs of gems.
+# Each pair must contain equal valued gems.
+# Return to the start point to get hasted.
+
+# This function returns two items with the same value.
+def findValuePair(items):
+    # Check each possible pair in the array.
+    # Iterate indexes 'i' from 0 to the second to last one.
+    for i in range(len(items) - 1):
+        # Iterate indexes 'j' from 'i+1' to the last.
+        for j in range(i + 1, len(items)):
+            # If we found a pair with two equal gems, then return them.
+            if items[i].value == items[j].value:
+                return [items[i], items[j]]
+    # Return an empty array if no pair exists.
+    return None
+
+while True:
+    gems = hero.findItems()
+    gemPair = findValuePair(gems)
+    # If the gemPair exists, collect the gems!
+    if gemPair:
+        # Move to the first gem first (gemPair[0]).
+        hero.moveXY(gemPair[0].pos.x, gemPair[0].pos.y)
+        # Return to get the haste from the wizard.
+        hero.moveXY(40, 44)
+        # Move to the second gem first (gempPair[1]).
+        hero.moveXY(gemPair[1].pos.x, gemPair[1].pos.y)
+        # Return to get the haste from the wizard.
+        hero.moveXY(40, 44)
+
---
+++ Mountain/PerimeterDefense.py
@@ -0,0 +1,28 @@
+#https://codecombat.com/play/level/perimeter-defense
+# We need to build guard towers around the village.
+# Each peasant can build one tower.
+# Show them the place to build.
+# These towers are automatic and will attack ALL units outside the town.
+
+# First move along the north border (y=60) from x=40 to x=80 with the step 20.
+# `range` doesn't include the second edge.
+for x in range(40, 81, 20):
+    # Move at each point and say anything.
+    hero.moveXY(x, 60)
+    hero.say("Here")
+# Next move along the east border (x=80) from y=40 to y=20 with the negative step -20.
+for y in range(40, 19, -20):
+    hero.moveXY(80, y)
+    hero.say("Here")
+# Continue for the two remaining borders.
+# Next the south border (y=20) from x=60 to x=20 with the negative step -20.
+for x in range(60, 19, -20):
+    hero.moveXY(x, 20)
+    hero.say("Here")
+# Next the west border (x=20) from y=40 to y=60 with the step 20.
+for y in range(40, 61, 20):
+    hero.moveXY(20, y)
+    hero.say("Here")
+
+# Don't forget hide inside the village.
+hero.moveXY(50, 40)
