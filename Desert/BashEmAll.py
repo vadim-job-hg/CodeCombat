b9996728c5fccde53fc370d540b378a33497634d
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-06-12 20:52:49 +0300

    #before-video

---
+++ Desert/BashEmAll.py
@@ -0,0 +1,17 @@
+#https://codecombat.com/play/level/bash-em-all?
+# Будь осторожен с ограми .
+# Это специально обученные, обезжиренное убер-огры. Они очень сильны.
+# Ключевое слово: "обезжиренные"
+loop:
+    enemy = hero.findNearest(hero.findEnemies())
+    if enemy:
+        if(hero.isReady('bash')):
+            hero.bash(enemy)
+        else:
+            hero.moveXY(40, 34)
+    else:
+        item = hero.findNearest(hero.findItems())
+        if item:
+            hero.moveXY(item.pos.x, item.pos.y)
+            hero.moveXY(40, 34)
+
