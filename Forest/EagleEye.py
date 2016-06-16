74469c81a1d334a9f5c8784a5d0e2b6fbf6d6b2d
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-06-16 18:28:17 +0300

    #cursed-valley

---
+++ Forest/EagleEye.py
@@ -0,0 +1,9 @@
+#https://codecombat.com/play/level/eagle-eye
+# This baby griffin will help you spot Ogres!
+
+while True:
+    target = hero.findNearestEnemy()
+    # Some of the targets will be Burls. You don't want to attack them!
+    # Use this to tell if you should attack:  hero.isOgre(target)
+    if target and target.type!='burl': # ∆ Change this!
+        hero.attack(target)
