fd908038d6cf9a6bf44230736523e6792c4b0468
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-07-03 10:35:49 +0300

    #new levels

---
+++ Forest/BadNeighborhood.py
@@ -0,0 +1,7 @@
+#https://codecombat.com/play/level/bad-neighborhood
+# Only attack ogres who get close to you.
+while True:
+    target = hero.findNearestEnemy()
+    # Use this to tell if you should attack:  hero.isClose(target)
+    if target and hero.distanceTo(target)<5: # ∆ Change this!
+        hero.attack(target)
