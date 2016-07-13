7adff9d00b4b151fd73bf0f3d98cf91af846a5d0
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-07-13 18:57:45 +0300

    #cursed-valley

--- Glacier/GoldenChoice.py
+++ Glacier/GoldenChoice.py
@@ -39,16 +39,23 @@ Here I need to return the route with summ also, but i will newer do that, beacus
 ###############################################
 kstart = []
 for kstart[0] in range(0, 19, 2): #sorry for that
-    if Math.abs(kstart[0]-kstart[1])==1:
-        for kstart[1] in range(1, 19, 2):#i am really sorry
-            if Math.abs(kstart[1] - kstart[2]) == 1:
-                for kstart[2] in range(0, 19, 2):#but i want to pass this level
+    for kstart[1] in range(1, 19, 2):#i am really sorry
+        if Math.abs(kstart[0] - kstart[1]) == 1:
+            for kstart[2] in range(0, 19, 2):#but i want to pass this level
+                if Math.abs(kstart[1] - kstart[2]) == 1:
                     for kstart[3] in range(1, 19, 2):#the best code to slow
-                        for kstart[4] in range(0, 19, 2):#i never do this again
-                            for kstart[5] in range(1, 19, 2):#thats such a shame
-                                for kstart[6] in range(0, 19, 2):#i was drunk when i tipe it
-                                    for kstart[7] in range(1, 19, 2):#realy realy
-                                        for kstart[8] in range(0, 19, 2):#I swear!
-                                            for kstart[9] in range(1, 19, 2):#nex time i will use recursion
-                                                for y in range(0, 9):
-                                                    pass
+                        if Math.abs(kstart[2] - kstart[3]) == 1:
+                            for kstart[4] in range(0, 19, 2):#i never do this again
+                                if Math.abs(kstart[3] - kstart[4]) == 1:
+                                    for kstart[5] in range(1, 19, 2):#thats such a shame
+                                        if Math.abs(kstart[4] - kstart[5]) == 1:
+                                            for kstart[6] in range(0, 19, 2):#i was drunk when i tipe it
+                                                if Math.abs(kstart[5] - kstart[6]) == 1:
+                                                    for kstart[7] in range(1, 19, 2):#realy realy
+                                                        if Math.abs(kstart[6] - kstart[7]) == 1:
+                                                            for kstart[8] in range(0, 19, 2):#I swear!
+                                                                if Math.abs(kstart[7] - kstart[8]) == 1:
+                                                                    for kstart[9] in range(1, 19, 2):#nex time i will use recursion
+                                                                        if Math.abs(kstart[8] - kstart[9]) == 1:
+                                                                            for y in range(0, 9):
+                                                                                pass
