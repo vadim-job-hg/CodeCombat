3e3a49ee3f4bac03a6651ff3a1cdb906b7fbfa07
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-07-14 18:48:17 +0300

    #cursed-valley

---
+++ DungeonsKitgard/MightierThanTheSword.py
@@ -0,0 +1,20 @@
+#https://codecombat.com/play/level/mightier-than-the-sword
+
+# A variable is a way of holding on to a value.
+# Here the "password" variable holds the secret phrase we need.
+password = 'Secret Message'
+hero.moveUp()
+hero.moveRight()
+hero.say(password)
+
+# A variable changes its value whenever you assign it.
+password = 'So Many Doors'
+hero.moveRight()
+hero.say(password)
+# Change the string in this line to the password variable.
+hero.say(password) # ∆ Change this!
+password = 'Let Me Out Of Here'
+# Move to the last door and say the password variable to open it.
+hero.moveRight()
+hero.say(password)
+hero.moveRight()
