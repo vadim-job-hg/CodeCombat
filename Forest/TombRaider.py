2868df1f349ebdf134f71f64258bcf2e64d6fd21
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-07-22 13:12:37 +0300

    #backwoods-fork

---
+++ Forest/TombRaider.py
@@ -0,0 +1,21 @@
+# https://codecombat.com/play/level/tomb-raider
+# A forgotten tomb in the forest!
+# But the ogres are hot on your heels.
+# Break open the tomb, while defending yourself from the munchkins.
+
+# This function should attack an enemy if it exists, otherwise attack the door!
+def checkToDefend(target):
+    # Check if the target exists
+    if target:
+    # If so, attack the target
+        hero.attack(target)
+    # Use an else to do something if there is no target
+    else:
+    # Otherwise attack the "Door"
+        hero.attack("Door")
+    pass
+
+
+while True:
+    enemy = hero.findNearestEnemy()
+    checkToDefend(enemy)
