c7648c34075f546020eeb2d593835e5a2f4927ae
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-07-28 15:33:09 +0300

    metal-detector

---
+++ Forest/CoinHunter.py
@@ -0,0 +1,29 @@
+#https://codecombat.com/play/level/coin-hunter
+# If you want to be a good hunter you need be patient.
+# To make the training more interesting Senick poisoned you.
+# While you aren't moving the poison is harmless.
+
+
+# This function checks if a coin is closer than 20 meters.
+def isCoinClose(coin):
+    # Find the distance to the coin.
+    distance = hero.distanceTo(coin)
+    # If the distance less than 20:
+    if distance<20:
+        # Return True
+        return True
+    # Else:
+    else:
+        # Return False
+        return False
+    pass
+
+while True:
+    #item = hero.findNearestItem()
+    item = hero.findNearest(hero.findItems())
+    if item:
+        # If the item is close enough, then move and take it.
+        if isCoinClose(item):
+            # Pick up the item.
+            hero.moveXY(item.pos.x, item.pos.y)
+            pass
---
+++ Forest/HitAndFreeze.py
@@ -0,0 +1,26 @@
+#https://codecombat.com/play/level/hit-and-freeze
+# You are trapped, but still dangerous.
+# Don't do any unnecessary movements, it'll be painful.
+# Hit ogres only they are near you and you can reach them with your sword.
+
+# This function check if the 'enemy' in your attack range.
+# The function returns a boolean value: True or False
+def inAttackRange(enemy):
+    distance = hero.distanceTo(enemy)
+    # Almost all swords have attack range equal 3.
+    if distance <= 3:
+        return True
+    else:
+        return False
+
+while True:
+    # Find the nearest enemy and store it in the variable.
+    #enemy = hero.findNearestEnemy()
+    enemy = hero.findNearest(hero.findEnemies())
+    # Call 'inAttackRange' function with the parameter from the previous step.
+    # Save the result in a variable ('canAttack' for example).
+    canAttack = inAttackRange(enemy)
+    # If the result of the function is true, then attack the enemy.
+    if canAttack:
+        hero.attack(enemy)
+    pass
