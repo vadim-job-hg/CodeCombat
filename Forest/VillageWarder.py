6e0ebe645b8b00eb3e283d0480ac7c934bfe5567
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-07-28 14:33:57 +0300

    metal-detector

---
+++ Forest/VillageWarder.py
@@ -0,0 +1,24 @@
+#https://codecombat.com/play/level/village-warder
+# This function attacks the nearest enemy.
+def findAndAttackEnemy():
+    enemy = hero.findNearestEnemy()
+    if enemy:
+        hero.attack(enemy)
+
+def findAndCleaveEnemy():
+    # Define a function to cleave enemies (but only when the ability is ready).
+    enemy = hero.findNearestEnemy()
+    if enemy and hero.isReady('cleave'):
+        hero.cleave(enemy)
+    pass
+
+# In your main loop, patrol, cleave, and attack.
+while True:
+    # Move to the patrol point, cleave, and attack.
+    hero.moveXY(35, 34)
+    findAndCleaveEnemy()
+    findAndAttackEnemy()
+    hero.moveXY(60, 31)
+    # Move to the other point, cleave, and attack.
+    findAndCleaveEnemy()
+    findAndAttackEnemy()
