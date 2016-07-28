deb6e5ef4bf5270f5011c90c1aa8cab58f2f1307
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-07-28 15:14:51 +0300

    metal-detector

---
+++ Forest/VillageChampion.py
@@ -0,0 +1,25 @@
+#https://codecombat.com/play/level/village-champion
+# Incoming munchkins! Defend the town!
+
+# Define your own function to fight the enemy!
+# In the function, find an enemy, then cleave or attack it.
+def attttaaaaacccckkkk():
+    enemy =  hero.findNearest(hero.findEnemies())
+    #enemy = hero.findNearestEnemy()
+    if enemy:
+        if enemy and hero.isReady('cleave'):
+            hero.cleave(enemy)
+        else:
+            hero.attack(enemy)
+
+# Move between patrol points and call the function.
+while True:
+    hero.moveXY(35, 34)
+    # Use whatever function name you defined above.
+    attttaaaaacccckkkk()
+    hero.moveXY(47, 27)
+    # Call the function again.
+    attttaaaaacccckkkk()
+    hero.moveXY(60, 31)
+    # Call the function again.
+    attttaaaaacccckkkk()
