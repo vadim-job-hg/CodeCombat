93b913a189209f2f0803947c3628e72374b68a0a
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-08-04 18:32:28 +0300

    ######

---
+++ Forest/GoFetch.py
@@ -0,0 +1,18 @@
+#https://codecombat.com/play/level/go-fetch
+#Commented out to stop infinite loop.
+# You've been caught in a burl trap!
+# Send your pet to fetch the health potions!
+
+def goFetch():
+    # You can use loops in a handler function.
+    while True:
+        #potion = hero.findNearestItem()
+        potion = hero.findNearest(hero.findItems())
+        if potion:
+            # Use pet.fetch() to have your pet fetch a potion:
+            pet.fetch(potion)
+            pass
+
+# When your pet is summoned, it triggers a "spawn" event.
+# This tells your pet to run goFetch() at the start of the level
+pet.on("spawn", goFetch)
