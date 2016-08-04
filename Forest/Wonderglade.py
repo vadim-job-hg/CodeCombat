195f716be5de4fd353e364d2e6bfdd2010c543e6
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-08-04 18:21:30 +0300

    ######

---
+++ Forest/Wonderglade.py
@@ -0,0 +1,12 @@
+# https://codecombat.com/play/level/wonderglade
+# You need to collect several items.
+# But, the burl wants the gems!
+# Pick up all appearing items EXCEPT gems.
+
+while True:
+    # item = hero.findNearestItem()
+    item = hero.findNearest(hero.findItems())
+    if item and item.type != 'gem':
+        # If item.type isn't equal "gem":
+        hero.moveXY(item.pos.x, item.pos.y)
+
