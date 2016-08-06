e3222771c439f4b2c487d30e1c0b255b1e296aa3
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-08-06 15:12:26 +0300

    FONDLED THE CODE

---
+++ Forest/ForestShadow.py
@@ -0,0 +1,18 @@
+#https://codecombat.com/play/level/forest-shadow
+# Big ogres can't see you in the forest.
+# Kill only the small ogres in the forest.
+# Collect coins and gems only.
+# Don't leave the forest and don't eat/drink anything.
+
+while True:
+    # Find the nearest enemy.
+    # Attack it only if its type is "thrower" or "munchkin".
+    enemy = hero.findNearestEnemy()
+    if enemy.type == 'thrower' or enemy.type == 'munchkin':
+        hero.attack(enemy)
+    # Find the nearest item.
+    # Collect it only if its type is "gem" or "coin".
+    item = hero.findNearestItem()
+    if item.type == 'gem' or item.type == 'coin':
+        hero.moveXY(item.pos.x, item.pos.y)
+    pass
---
+++ Forest/StarShower.py
@@ -0,0 +1,17 @@
+#https://codecombat.com/play/level/star-shower
+# A star shower is a time when gems and coins appear around you.
+# Metal coins disappear quickly, run for them only they are close (< 20m).
+# Gems don't disappear, run for them every time when you see them.
+
+while True:
+    pass
+    # Find the nearest item.
+    item = hero.findNearestItem()
+    # Find the distance between the hero and the item.
+    if item:
+        distance = hero.distanceTo(item)
+        # If the item's type is 'gem' OR the distance to the item less than 20 metres:
+        if item.type == 'gem' or distance < 20:
+            # Take it.
+            hero.moveXY(item.pos.x, item.pos.y)
+
