93efded927cefe4d716925adc8bd88d57c1838bc
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-08-07 13:03:53 +0300

    FONDLED THE CODE

---
+++ DungeonsKitgard/EvesCodecombat.py
@@ -0,0 +1,9 @@
+#https://codecombat.com/play/level/eves-codecombat
+# Attack the door.
+# Собери все самоцветы, используя команды перемещения.
+hero.attack('Door')
+hero.moveRight()
+hero.moveDown()
+hero.moveUp()
+hero.moveUp()
+hero.moveRight()
