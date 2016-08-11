722488a6a443306bf4d465c5e0f619b684759ab7
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-08-11 18:31:16 +0300

    ######

---
+++ Forest/AgrippaRefactored.py
@@ -0,0 +1,23 @@
+#https://codecombat.com/play/level/agrippa-refactored
+def cleaveOrAttack(enemy):
+    # If "cleave" is ready, cleave; otherwise, attack.
+    # enemy = self.findNearestEnemy()
+    enemy = self.findNearest(self.findEnemies())
+    if (enemy):
+        dist = self.distanceTo(enemy)
+        if (dist < 5):
+            if (self.isReady("cleave")):
+                self.cleave(enemy)
+            else:
+                self.attack(enemy)
+
+    pass
+
+
+while True:
+    enemy = self.findNearestEnemy()
+    if enemy:
+        distance = self.distanceTo(enemy)
+        if distance < 5:
+            # Call the "cleaveOrAttack" function, defined above.
+            cleaveOrAttack(enemy)
