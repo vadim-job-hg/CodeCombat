ce525abc95f0c3485e2d341786379695fb6e1172
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-08-12 11:36:39 +0300

    gameDev

---
+++ Forest/BrawlerHunt.py
@@ -0,0 +1,14 @@
+#https://codecombat.com/play/level/brawler-hunt
+# Огры идут по одному.
+# Не беспокойся по поводу мелких и средних огров.
+# Твои цели - это драчуны (brawler)
+# Подожди пока драчун достаточно близко (<50м) и прикажи стрелять.
+
+while True:
+    # Найди ближайшего врага и расстояние до него.
+    enemy = hero.findNearestEnemy()
+    # Если тип (type) огра равен "brawler" и расстояние до него меньше 50 метров.
+    # Тогда скажи что угодно. Это будет сигнал для артиллерии, чтобы стрелять.
+    if enemy  and enemy.type=='brawler' and hero.distanceTo(enemy)<50:
+        hero.say('something')
+    pass
