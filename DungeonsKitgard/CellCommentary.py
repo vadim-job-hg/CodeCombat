6399eacb8e0d317299054d4e4ed1198bfaba4b8d
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-08-12 12:43:21 +0300

    gameDev

---
+++ DungeonsKitgard/CellCommentary.py
@@ -0,0 +1,12 @@
+#https://codecombat.com/play/level/cell-commentary
+# Comments are a way for programmers to explain their code to others.
+# In Python, comments start with # and last until the end of the line.
+# Read the comments below and do what they say to escape the prison cell.
+
+# Use the "say()" function to say the password.
+# The password is: "Achoo"
+hero.say("Achoo")
+# Use the "moveUp()" function twice to escape.
+hero.moveUp()
+hero.moveUp()
+
