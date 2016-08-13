b2bf7b39554f185c784f942c68bb62f57de2a417
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-08-13 14:29:25 +0300

    FONDLED THE CODE

---
+++ Forest/PatrolBusterA.py
@@ -0,0 +1,8 @@
+#https://codecombat.com/play/level/patrol-buster-a
+# Помните, что врага в данный момент может не быть рядом.
+while True:
+    enemy = hero.findNearestEnemy()
+    if enemy:
+        # Но когда он появится, атакуйте!
+        hero.attack(enemy)
+        pass
