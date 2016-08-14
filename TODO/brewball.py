64bb7a40fa97a0cab1dccddc358a3b6ddb02810a
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-08-14 20:16:46 +0300

    FONDLED THE CODE

--- TODO/brewball.py
+++ TODO/brewball.py
@@ -1,46 +1,41 @@
-# http://codecombat.com/play/level/brewball?session=56c5ebfded946a44004fb659&observing=true
-# todo:
 # Скажи кое-что в расстоянии 10м от Омарна чтобы он кинул зелье.
 # Поймай зелье стоя рядом с ним прежде чем он упадет.
 # НЕ ДАЙ ЗЕЛЬЮ УПАСТЬ НА ЗЕМЛЮ!
-def mineGo(trap, go):
-    pass
-
-
+def arowndMine(move, trap, radius = 3):
+    way = Vector.subtract(hero.pos, trap.pos)
+    normal = Vector.normalize(way)
+    direction = Vector.multiply(normal, radius+1)
+    wayCorr =  Vector.add(trap.pos, direction)
+    dot = Vector.normalize(move)
+    dot = Vector.add(wayCorr, dot)
+    way = Vector.subtract(dot, trap.pos)
+    normal = Vector.normalize(way)
+    direction = Vector.multiply(normal, radius+2)
+    wayCorr =  Vector.add(trap.pos, direction)
+    return wayCorr
+home = Vector(14, 34)
 while True:
     potion = hero.findFriendlyMissiles()[0]
     firetraps = hero.findHazards()
-    start = Vector(9, 34)
     # Запомни что огненная ловушка сработает если ты подойдешь ближе чем 3 метра!
     omarn = hero.findByType("potion-master")[0]
     if potion:
-        dest = potion.targetPos
-        go = self.pos
-        add = Vector.subtract(potion.pos, go)
-        add = Vector.normalize(add)
-        add = Vector.multiply(add, 1)
-        go = Vector.add(add, go)
+        dest = potion.targetPos;
+        direction = Vector.multiply(Vector.normalize(Vector.subtract(dest, hero.pos)), 3)
+        move = Vector.add(hero.pos, direction)
         trap = self.findNearest(firetraps)
-        if trap and self.distanceTo(trap) < 6:
-            go = mineGo(trap, go)
-        self.move(go)
+        if trap and self.distanceTo(trap) < 4:
+            move = arowndMine(move, trap)
+        hero.move(move)
     else:
         if omarn and hero.distanceTo(omarn) > 10:
             # Вернись к Омарну.
-            go = self.pos
-            add = Vector(14, 34)
-            add = Vector.subtract(go, add)
-            add = Vector.normalize(add)
-            add = Vector.multiply(add, 1)
-            go = Vector.add(add, go)
+            direction = Vector.multiply(Vector.normalize(Vector.subtract(home, hero.pos)), 10)
+            move = Vector.add(hero.pos, direction)
+            # Предупреждение: isPathClear не работает с Радиацией!
             trap = self.findNearest(firetraps)
             if trap and self.distanceTo(trap) < 4:
-                vector = Vector.subtract(self.pos, trap.pos)
-                vector = Vector.normalize(vector)
-                vector = Vector.multiply(vector, 5)
-            go = Vector.add(vector, go)
-            self.move(go)
-            # Предупреждение: isPathClear не работает с Радиацией!
-            pass
+                move = arowndMine(move, trap)
+            hero.move(move)
         else:
             hero.say("Hup, hup!")
