caa27dad6c0c0b02bbe93a70a13425a49c81c0e3
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-08-18 11:02:28 +0300

    Fixes

---
+++ Forest/ShortSightedBurl.py
@@ -0,0 +1,22 @@
+#https://codecombat.com/play/level/short-sighted-burl
+# The gold is guarded by a burl.
+# The burl is short-sighted but it has good hearing.
+# Collect coins and run unless the burl will find you.
+
+# Write the function "checkTakeRun".
+# It should take one parameter -- an item.
+# If the item exists, take it.
+# Move to the start point (the green mark) whether the item or no.
+def checkTakeRun(item):
+    if item:
+        hero.moveXY(item.pos.x, item.pos.y)
+    hero.moveXY(40, 12)
+
+# Don't change this code. Just write the function.
+while True:
+    hero.moveXY(16, 56)
+    item = hero.findNearestItem()
+    checkTakeRun(item)
+    hero.moveXY(64, 56)
+    item = hero.findNearestItem()
+    checkTakeRun(item)
---
+++ Forest/TombGhost.py
@@ -0,0 +1,17 @@
+#https://codecombat.com/play/level/tomb-ghost
+# The only exit is blocked by ogres.
+# Hide from the skeletons and kill ogres one by one.
+
+# This function should attack an enemy and hide.
+def hitOrHide(target):
+    # If 'target' exists:
+    if target:
+        hero.attack(target)
+        # Attack 'target' and then move to the red mark.
+    else:
+        hero.moveXY(32, 17)
+    pass
+
+while True:
+    enemy = hero.findNearestEnemy()
+    hitOrHide(enemy)
