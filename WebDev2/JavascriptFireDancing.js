c11c908c4a58ad52864e1e3f3a859b8337b44ce7
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-08-23 11:11:38 +0300

    #gameDev

---
+++ WebDev2/JavascriptFireDancing.js
@@ -0,0 +1,9 @@
+// https://codecombat.com/play/level/javascript-fire-dancing
+// Обычно код выполняется в том порядке, в каком он написан.
+// Циклы позволяют повторять кусок твоего кода множество раз.
+
+while(true) {
+    hero.moveRight();
+    // Комментарии ниже покажут как сделать цикл.
+    hero.moveLeft();
+}
