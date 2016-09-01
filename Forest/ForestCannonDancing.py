b1339eb736a3f494290f1490761113024034e082
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-09-01 23:16:57 +0300

    game Dev

---
+++ Forest/ForestCannonDancing.py
@@ -0,0 +1,17 @@
+#https://codecombat.com/play/level/forest-cannon-dancing
+# Your pet should run back and forth on the marks.
+# The hero should cheer the whole time!
+
+# Write the event function "runBetween" for the pet.
+# This function should make the wolf run back and forth:
+# First, the pet should run to the right mark, then the left one:
+def runBetween(event):
+    while True:
+        pet.moveXY(48,8)
+        pet.moveXY(12,8)
+
+pet.on("spawn", runBetween)
+# Cheer up your pet. Don't remove this:
+while True:
+    hero.say("Run!!!")
+    hero.say("Faster!")
---
+++ Forest/ForestJogging.py
@@ -0,0 +1,26 @@
+#https://codecombat.com/play/level/forest-jogging
+# Let's train your pet a little.
+
+# This function makes a pet run around.
+def runAround(event):
+    while True:
+        # First, move to the left mark:
+        pet.moveXY(9, 24)
+        # Next, the top mark:
+        pet.moveXY(30, 43)
+        # Move your pet to the right mark:
+        pet.moveXY(51, 24)
+        # Finally move your pet to the bottom mark:
+        pet.moveXY(30, 5)
+
+# Assign the "runAround" function for the pet's event "spawn":
+pet.on("spawn", runAround)
+
+# Cheer on your pet!
+# Don't remove "say" commands below.
+while True:
+    hero.say("Good dog!")
+    hero.say("You can do it!")
+    hero.say("Run Run Run!")
+    hero.say("Almost!")
+    hero.say("One more lap!")
