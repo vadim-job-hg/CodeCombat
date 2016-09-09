1388210004f03ab87e09e5ad9bf887c06e5b920c
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-09-09 12:30:49 +0300

    fast-and-furry-ous

---
+++ Desert/NoPainNoGain.py
@@ -0,0 +1,9 @@
+#https://codecombat.com/play/level/no-pain-no-gain
+# Run while you can, but watch your health.
+
+# While hero's health greater than 200:
+while hero.health>200: # Δ Change this!
+    hero.moveXY(48, 24)
+    hero.moveXY(16, 24)
+# Move to Okar.
+hero.moveXY(32, 40);
