a693b50dcefe20d9097a14b614cf9c1171cb41ba
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-09-09 17:58:21 +0300

    fast-and-furry-ous

---
+++ Forest/CatchMe.py
@@ -0,0 +1,15 @@
+#https://codecombat.com/play/level/catch-me
+# Protect the village from ogres catapults.
+# Be aware of shamans and don't stand out the village.
+
+# Write the function "hitAndRun".
+# If there is a target, then hit it and run in the village.
+def hitAndRun():
+    enemy = hero.findNearest(hero.findEnemies())
+    if enemy and enemy.pos.y>20 and enemy.pos.y<30:
+        hero.attack(enemy)
+        hero.moveXY(44, 25)
+
+while True:
+    enemy = hero.findNearestEnemy()
+    hitAndRun(enemy)
