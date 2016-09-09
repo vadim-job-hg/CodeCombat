1fb9d4e28d80f1026bbe92d94992820b0f87ff1e
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-09-09 12:20:20 +0300

    fast-and-furry-ous

---
+++ Desert/SpinachPower.py
@@ -0,0 +1,21 @@
+#https://codecombat.com/play/level/spinach-power
+# Collect exactly no more than 7 spinach potions.
+# With these potions you'll be strong enough to kill ogres.
+
+potionCount = 0
+
+# Use while with a condition to check the number of collected potions.
+# Δ Wrap the next code block into while loop.
+while potionCount != 7:
+    item = hero.findNearest(hero.findItems())
+    if item:
+        hero.moveXY(item.pos.x, item.pos.y)
+        potionCount += 1
+
+# When the while loop is finished.
+# Go and fight!.
+while True:
+    enemy = hero.findNearest(hero.findEnemies())
+    if enemy:
+        hero.attack(enemy)
+
