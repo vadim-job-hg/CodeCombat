7b7faf097f46c2789543bfeb7ba9b3f87b53bae3
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-09-16 00:07:07 +0300

    game Dev

---
+++ Desert/CoordinatedDefense.py
@@ -0,0 +1,13 @@
+#https://codecombat.com/play/level/coordinated-defense
+# Protect peasants from ogres.
+
+while True:
+    # Get an array of enemies.
+    enemies = hero.findEnemies()
+    # If the array is not empty.
+    if len(enemies) > 0:
+        # Attack the first enemy from "enemies" array.
+        hero.attack(enemies[0])
+        # Return to the start position.
+        hero.moveXY(40, 20)
+        pass
---
+++ Desert/RecruitingQueue.py
@@ -0,0 +1,14 @@
+#https://codecombat.com/play/level/recruiting-queue
+# Call peasants one after another.
+
+# Neutral units are detected as enemies.
+neutrals = hero.findEnemies()
+while True:
+    if len(neutrals):
+        # Say the first peasant in the array of neutral units.
+        hero.say(neutrals[0].id)
+        pass
+    else:
+        hero.say("Nobody here")
+    # Reassign "neutrals" with "findEnemies" method again.
+    neutrals = hero.findEnemies()
