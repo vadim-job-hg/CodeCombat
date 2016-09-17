1c12d09db5c73288f91478a4f71371b0ff72bdae
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-09-17 14:24:35 +0300

    ha-ha

--- Mountain/AlmostPerfectMinefield.py
+++ Mountain/AlmostPerfectMinefield.py
@@ -1,1 +1,23 @@
 #https://codecombat.com/play/level/almost-perfect-minefield
+# Escape from the minefield.
+# You need to pass the marked line. (x = 72).
+
+trapDistance = 6
+trapRange = 2
+jump = ['right', 'right', 'up', 'up', 'left', 'up', 'up', 'right', 'right','right', 'right', 'right']
+index =  0
+# Jump, Jump, JUMP!!!
+while index<len(jump):
+    if(hero.isReady('jump')):
+        if jump[index]=='right':
+            where = {'x': (hero.pos.x +(trapDistance)*2), 'y':hero.pos.y}
+        elif jump[index]=='left':
+            where = {'x': (hero.pos.x -(trapDistance)*2), 'y':hero.pos.y}
+        elif jump[index]=='up':
+            where = {'x': hero.pos.x, 'y':(hero.pos.y +(trapDistance)*2)}
+        else:
+            where = {'x': hero.pos.x, 'y':(hero.pos.y - (trapDistance)*2)}
+        hero.jumpTo(where)
+        index = index +  1
+    else:
+        hero.move(where)
--- Multiplayer/README.md
+++ Multiplayer/README.md
@@ -3,3 +3,5 @@
 ![Cavern Survival](ladder/cavernsurv.png?raw=true "Cavern Survival")
 # Treasure Grove
 ![Cavern Survival](ladder/treasure.png?raw=true "Treasure Grove")
+# Capture their Flag
+![Cavern Survival](ladder/flag.png?raw=true "Capture their Flag")
