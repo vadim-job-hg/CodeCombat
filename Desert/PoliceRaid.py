67f4790afc901a50ab10768ee36391ec99a1d817
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-09-22 13:17:44 +0300

    pass

---
+++ Desert/PoliceRaid.py
@@ -0,0 +1,25 @@
+#https://codecombat.com/play/level/police-raid
+# Wait for ogres, defeat them and collect gold.
+
+while True:
+    enemies = hero.findEnemies()
+    # That variable is used to iterate "enemies" array.
+    enemyIndex = 0
+    # While it less than the array length:
+    while enemyIndex < len(enemies):
+        # Get an enemy from the array.
+        enemy = enemies[enemyIndex]
+        hero.attack(enemy)
+        # Increase "enemyIndex" by one.
+        enemyIndex += 1
+    coins = hero.findItems()
+    # That variable is used to iterate "coins" array.
+    coinIndex = 0
+    while coinIndex < len(coins):
+        # Get a coin from the "coins" array at coinIndex.
+        coin = coins[coinIndex]
+        # Collect that coin.
+        hero.moveXY(coin.pos.x, coin.pos.y)
+        # Increase "coinIndex" by one.
+        coinIndex = coinIndex + 1
+        pass
---
+++ Desert/WanderingSouls.py
@@ -0,0 +1,23 @@
+#https://codecombat.com/play/level/wandering-souls
+# Defeat skeletons and collect lightstones.
+
+while True:
+    enemies = hero.findEnemies()
+    enemyIndex = 0
+    while enemyIndex < len(enemies):
+        enemy = enemies[enemyIndex]
+        # Hit it while it has health
+        while enemy.health > 0:
+            hero.attack(enemy)
+        enemyIndex += 1
+    items = hero.findItems()
+    itemIndex = 0
+    # Iterate over all items.
+    while itemIndex < len(items):
+        item = items[itemIndex]
+        # While the distance greater than 4:
+        while item and hero.distanceTo(item)>4:
+            # Try to take the item.
+            hero.moveXY(item.pos.x, item.pos.y)
+        # Don't forget to increase "itemIndex".
+        itemIndex += 1
