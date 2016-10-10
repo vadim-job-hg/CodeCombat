05823ec170a8bbadbc0d507eab44a687b93d3809
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-10-10 12:45:00 +0300

    pets

---
+++ Desert/OlympicRace.py
@@ -0,0 +1,16 @@
+#https://codecombat.com/play/level/olympic-race
+# The pet must win the race.
+# Runners should touch a mark and run back.
+
+def waitAndRun(event):
+    referee = pet.findNearestByType("wizard")
+    # If the speaker is referee and the message is "Start":
+    if event.speaker==referee and event.message == 'Start':
+        # Make the pet run to the red mark.
+        pet.moveXY(54, 27)
+        # Then run back.
+        pet.moveXY(6, 28)
+
+# Assign the waitAndRun function to handle "hear" events.
+
+pet.on("hear", waitAndRun)
