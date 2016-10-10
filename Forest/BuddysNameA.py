b5d490f7c171c777706db8b6a1c659188cd95db1
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-10-10 12:05:21 +0300

    pets

---
+++ Forest/BuddysNameA.py
@@ -0,0 +1,12 @@
+#https://codecombat.com/play/level/buddys-name-a
+# The peasant need to know the name of the pet.
+
+# Use this function as a handler for "hear" events on pet.
+def saySomething(event):
+    # Make the pet say something.
+    pet.say('something')
+    pass
+
+# Use the pet.on(eventType, eventHandler) method
+# Assign the saySomething function to handle "hear" events.
+pet.on('hear', saySomething)
---
+++ Forest/BuddysNameB.py
@@ -0,0 +1,15 @@
+#https://codecombat.com/play/level/buddys-name-b
+# The pet should greet the hero and the peasant.
+
+# Use this function as a handler for "hear" events on pet.
+def sayHello(event):
+    # Make the pet say "Hello".
+    pet.say('Hello')
+    pass
+
+# Use the pet.on(eventType, eventHandler) method
+# Assign the sayHello function to handle "hear" events.
+pet.on('hear', sayHello)
+
+# It must be after "pet.on".
+hero.say("Hello, Kitty!")
