382e3cf84f987f23cf6bead58c5e47b0f3293a15
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-10-14 14:52:48 +0300

    pets

---
+++ Desert/MadMaxerRedemption.py
@@ -0,0 +1,17 @@
+#https://codecombat.com/play/level/mad-maxer-redemption
+# Вы не можете защитить своих друзей!
+# Прикажи им идти домой, где лучник сможет им помочь.
+
+while True:
+    weakestFriend = None
+    leastHealth = 9999
+    friendIndex = 0
+    # Найди у кого из друзей самый низкий уровень здоровья.
+    friends = hero.findFriends()
+    for friend in friends:
+        if leastHealth>friend.health:
+            weakestFriend = friend
+            leastHealth = friend.health
+    # Скажи другу с самым низким уровнем здоровья, идти домой первым.
+    if weakestFriend:
+        hero.say('Hey ' + weakestFriend.id + ', go home!')
