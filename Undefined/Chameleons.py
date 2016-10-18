6771e491d365060deba23d6cd3a23b105663ce91
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-10-18 13:49:00 +0300

    pets

---
+++ Undefined/Chameleons.py
@@ -0,0 +1,12 @@
+#https://codecombat.com/play/level/chameleons
+# Ogres are disguised as gems or coins.
+
+while True:
+    enemy = hero.findNearestEnemy()
+    # If you see an enemy - attack it.
+    if enemy:
+        hero.attack(enemy)
+    item = hero.findNearestItem()
+    # If you see an item - try to collect it.
+    if item:
+        hero.moveXY(item.pos.x, item.pos.y)
