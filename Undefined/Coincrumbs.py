980d6163628102cd57511120a3ea50351140f6d7
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-10-18 14:00:45 +0300

    pets

---
+++ Undefined/Coincrumbs.py
@@ -0,0 +1,14 @@
+#https://codecombat.com/play/level/coincrumbs
+# Follow by the coin trail and escape from the room.
+
+while True:
+    # Find the nearest item.
+    item = hero.findNearestItem()
+    if item:
+        # Get the position of the item.
+        itemPosition = item.pos
+        # Get X and Y coordinates of the item.
+        itemX = itemPosition.x
+        itemY = itemPosition.y
+        # To collect the item, move to the item.
+        hero.moveXY(itemX, itemY)
---
+++ Undefined/WhiteRabbit.py
@@ -0,0 +1,14 @@
+#https://codecombat.com/play/level/white-rabbit
+# Follow by the lightstone and escape from the room.
+
+while True:
+    item = hero.findNearestItem()
+    if item:
+        pass
+        # Get the item's position with the property "pos".
+        pos = item.pos
+        # Get "x" and "y" coordinates from the position.
+        x = pos.x
+        y = pos.y
+        # Move to the coordinates (x, y).
+        hero.moveXY(x, y)
