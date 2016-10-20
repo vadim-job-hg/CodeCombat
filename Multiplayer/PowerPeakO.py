bd15060e6a7896cd9e5b8c5f6708cf4413994a0d
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-10-20 10:50:16 +0300

    pets

--- Multiplayer/PowerPeakO.py
+++ Multiplayer/PowerPeakO.py
@@ -1,2 +1,26 @@
 # todo:
 # https://codecombat.com/play/level/power-peak?team=ogres&course=56462f935afde0c6fd30fc8c&course-instance=56fc25d7263b0220002aff0e
+def petLogic():
+    # Add code to use your pet!
+    # Move them to the power discs at the top of the map for powerups.
+    while True:
+        pet.moveXY(51, 74);
+        item = pet.findNearestItem()
+        if item:
+            pet.fetch(item)
+
+
+pet.on('spawn', petLogic)
+while True:
+    # Improve your hero's default code!
+    # Move to the nearby power discs to spawn units to help or attack!
+
+    enemy = hero.findNearestEnemy()
+    item = hero.findNearestItem()
+    if enemy:
+        if hero.isReady('cleave'):
+            hero.cleave(enemy)
+        else:
+            hero.attack(enemy)
+    elif item:
+        hero.moveXY(item.pos.x, item.pos.y)
