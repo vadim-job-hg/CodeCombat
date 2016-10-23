1929d87e22ef0c45268a096bf3df50c4d71f76e4
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2016-10-23 17:54:09 +0300

    Create EchoOfWar.py

---
+++ Mountain/EchoOfWar.py
@@ -0,0 +1,24 @@
+#https://codecombat.com/play/level/echo-of-war
+# Destroy 5 robobombs. Some of them are old and safe.
+# Old (safe) bombs have the certain letter in their id.
+
+# This function check if the letter in the word.
+def isLetterInWord(word, letter):
+    # Complete the function.
+    for index in range(len(word)):
+        if word[index] == letter:
+            return True
+    return False
+
+# The engineer knows how the old robots are marked.
+engineer = hero.findFriends()[0]
+safeLetter = engineer.safeLetter
+
+enemies = hero.findEnemies()
+for index in range(len(enemies)):
+    enemy = enemies[index]
+    if isLetterInWord(enemy.id, safeLetter):
+        # Destroy the enemy if it's safe.
+        while enemy.health > 0:
+            hero.attack(enemy)
+
