ca927ac90dde2ef3332aa33b83b3c7b17dfdb43d
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-10-29 15:01:31 +0300

    northwest

--- Mountain/IceHunter.py
+++ Mountain/IceHunter.py
@@ -1,2 +1,14 @@
-#todo:
 #https://codecombat.com/play/level/ice-hunter
+# Should fill in some default source
+
+while True:
+    enemies = hero.findEnemies()
+    health = 999999
+    target = None
+    if len(enemies)>0:
+        for enemy in enemies:
+            if enemy.health<health and enemy.type=='ice-yak':
+                health = enemy.health
+                target = enemy
+        if target:
+            hero.attack(target)
---
+++ Mountain/YetiBeater.py
@@ -0,0 +1,23 @@
+#https://codecombat.com/play/level/yeti-beater
+# Follow by one of the peasants and escape from yetis.
+
+def startsWith(phrase, word):
+    if len(word) > len(phrase):
+        return False
+    # Iterate indexes from 0 to the length of the word.
+    for index in range(0, len(word)):
+        # If characters with the same index
+        # in the word and in the phrase are not equal.
+        if word[index]!=phrase[index]:
+            # Then return False.
+            return False
+    # All letters in the word checked, then return true.
+    return True
+
+# Follow by a local guide whose name starts with "Mac".
+guides = hero.findFriends()
+for gIndex in range(len(guides)):
+    guide = guides[gIndex]
+    if startsWith(guide.id, "Mac"):
+        while True:
+            hero.move(guide.pos)
