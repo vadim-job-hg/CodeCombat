2446fb3f6b94048b06a53a023cc2f94c2906f8ac
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-11-03 16:01:22 +0200

    gameDev

---
+++ undefined/Jailbreak.py
@@ -0,0 +1,7 @@
+# https://codecombat.com/play/level/jailbreak
+# The player only needs to defeat some of the enemies.
+game.spawnHeroXY(62, 25);
+
+# Use game.addDefeatGoal(X) to allow the player to win
+# after defeating X enemies! (In this case, X should be 4)
+game.addDefeatGoal(4)
