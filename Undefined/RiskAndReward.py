46005fdc143070e56a069f6a4177358d8590d9a0
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-11-07 10:00:28 +0300

    northwest

---
+++ Undefined/RiskAndReward.py
@@ -0,0 +1,8 @@
+#https://codecombat.com/play/level/risk-and-reward
+# The player only needs to collect some of the gems.
+game.spawnHeroXY(39, 23)
+
+# Use game.addCollectGoal(X) to allow the player to win
+# after collecting only X gems! (In this case, X should be 6)
+game.addCollectGoal(6)
+
