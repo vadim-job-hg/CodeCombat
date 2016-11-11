d3a83efb849342c3b9560379184dfead34e980a8
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-11-11 12:07:13 +0300

    cluttered-corridors

---
+++ Mountain/ReadingRumble.py
@@ -0,0 +1,14 @@
+#https://codecombat.com/play/level/reading-rumble
+# Defeat all incoming ogres.
+
+while True:
+    # Find the nearest enemy.
+    enemy = hero.findNearest(hero.findEnemies())
+    # If there is an enemy, and it is a "brawler":
+    if enemy and enemy.type == 'brawler':
+        # Then say its name (.id) in UPPERCASE.
+        hero.say(enemy.id.toUpperCase())
+    # For other enemies, just fight.
+    elif enemy:
+        hero.attack(enemy)
+    pass
