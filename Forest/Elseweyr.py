bb4408f3db0efaa0b473bcc0bc8710a873bdcd81
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-11-11 10:26:38 +0300

    elseweyr

---
+++ Forest/Elseweyr.py
@@ -0,0 +1,14 @@
+#https://codecombat.com/play/level/elseweyr
+# Your cleave is on a 10 second cooldown.
+# Use an else-statement to defend yourself while recharging.
+
+while True:
+    enemy = hero.findNearestEnemy()
+    if hero.isReady("cleave"):
+        hero.cleave()
+    # Write else: to do something when cleave isn't ready:
+    else:
+        # Be sure to attack the enemy:
+        if enemy:
+            hero.attack(enemy)
+
