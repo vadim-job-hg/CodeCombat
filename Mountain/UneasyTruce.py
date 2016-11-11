cffd2d2cf01916f574aa4ac4a8889a806d2cb0a2
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-11-11 15:39:06 +0300

    #

---
+++ Mountain/UneasyTruce.py
@@ -0,0 +1,24 @@
+#https://codecombat.com/play/level/uneasy-truce
+# Summon one soldier for every ogre to the south of you!
+# Don't count the ogres to the north!
+
+# Accept an array of units as the parameter.
+# Return only the units to the south of the hero.
+def findSouthernUnits(units):
+    southernUnits = []
+    for unit in units:
+        if unit.pos.y < hero.pos.y:
+            # Add the unit to the array with: append()
+            southernUnits.append(unit)
+    return southernUnits
+
+
+while True:
+    friends = hero.findFriends()
+    enemies = hero.findEnemies()
+    # Use findSouthernUnits to get enemies to the south.
+    ogres = findSouthernUnits(enemies)
+    # If there are more ogres south of you than friends.
+    if(len(ogres)>len(friends)):
+        # Then summon another "soldier".
+        hero.summon("soldier")
