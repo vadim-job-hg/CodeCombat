a50f9c4850b544f2242791babdd4379980428785
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-11-12 13:46:46 +0300

    #

---
+++ Mountain/PerilousPassage.py
@@ -0,0 +1,26 @@
+#https://codecombat.com/play/level/perilous-passage
+# Rally the peasants to protect their home!
+# Have them build only 1 trap between them and the enemy.
+def findTheMiddle(pos1, pos2):
+    return {'x': (pos1.x + pos2.x) / 2, 'y': (pos1.y + pos2.y) / 2}
+friends = hero.findFriends()
+for friend in friends:
+    xPos = friend.pos.x
+    yPos = friend.pos.y
+    friend["startPos"] = {"x":xPos, "y":yPos}
+
+while True:
+    enemies = hero.findEnemies()
+    for enemy in enemies:
+        friend = enemy.findNearest(friends)
+        if hero.isPathClear(friend.pos, enemy.pos):
+            if not enemy.accountedFor:
+                # Command the friend to build a firetrap
+                # at the midpoint between them and enemy.
+                mid = findTheMiddle(friend.pos, enemy.pos)
+                hero.command(friend, 'buildXY', 'fire-trap', mid.x, mid.y)
+                enemy["accountedFor"] = True;
+        else:
+            # Otherwise, command the friend to move back
+            # to their .startPos property:
+            hero.command(friend, 'move',friend["startPos"] )
