3692fe7ef56196fffbf0a66cecc33c571c74db1a
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-11-12 14:04:00 +0300

    #

---
+++ DungeonsKitgard/SleepingOnTheJob.py
@@ -0,0 +1,14 @@
+#https://codecombat.com/play/level/sleeping-on-the-job
+
+# The guards protect the buttons to the door.
+
+# Move through the dungeons to the buttons:
+hero.moveUp()
+hero.moveRight(2)
+# Defeat the ogres by attacking them twice:
+hero.attack('Vera')
+hero.attack('Vera')
+hero.attack('Bas')
+hero.attack('Bas')
+# Move to the gem and escape:
+hero.moveRight(2)
