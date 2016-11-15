365c52ed3baf3c4c2283ac52b35ed097ff686617
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-11-15 20:29:56 +0200

    #

---
+++ Desert/DesertDelta.py
@@ -0,0 +1,11 @@
+#https://codecombat.com/play/level/desert-delta
+# Only attack the enemies in the enemyNames array.
+# Be sure to attack in order! 0 -> 1 -> 2 -> 3
+enemyNames = ["Kog", "Godel", "Vorobun", "Rexxar"]
+
+hero.attack(enemyNames[0])
+hero.attack(enemyNames[1])
+# Attack enemyNames[2]:
+hero.attack(enemyNames[2])
+# Attack the last element:
+hero.attack(enemyNames[3])
