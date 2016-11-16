44da92d3a8185729452edf0030d988eac6017dbe
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-11-16 10:13:07 +0300

    triage

---
+++ Forest/IfStravaganza.py
@@ -0,0 +1,9 @@
+#https://codecombat.com/play/level/if-stravaganza
+# Defeat the ogres from within their own camp!
+
+while True:
+    enemy = hero.findNearestEnemy()
+    # Use an if-statement to check if an enemy exists:
+    if enemy:
+        # Attack the enemy if it exists:
+        hero.attack(enemy)
