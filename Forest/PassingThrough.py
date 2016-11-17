696c115143c9d23f89964814138c53c8347ced19
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-11-17 10:07:10 +0300

    triage

---
+++ Forest/PassingThrough.py
@@ -0,0 +1,16 @@
+#https://codecombat.com/play/level/passing-through
+# Don't insult this tribe of peaceful ogres.
+hero.wait(1)
+while True:
+    item = hero.findNearestItem()
+    if item:
+        # If item.type IS NOT EQUAL TO "gem"
+        if item.type != "gem":
+            # Then follow your pet wolf.
+            hero.moveXY(hero.pos.x, pet.pos.y)
+            hero.moveXY(pet.pos.x, pet.pos.y)
+        # Else:
+        else:
+            # Move to the gem's position.
+            hero.moveXY(item.pos.x, item.pos.y)
+
