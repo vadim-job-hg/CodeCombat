18a03ab7fc44a10cee44567ba06e3f09ee98890d
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-11-20 23:17:43 +0200

    #

---
+++ Undefined/ForestGarden.py
@@ -0,0 +1,17 @@
+#https://codecombat.com/play/level/forest-garden
+
+# Grow the perfect rectangular flower fence.
+
+# Use the certain sizes for the flower rectangular.
+gardener = hero.findNearest(hero.findFriends())
+gardenWidth = gardener.gardenWidth
+gardenHeight = gardener.gardenHeight
+# Start grow from the initial position.
+hero.toggleFlowers(True)
+x = hero.pos.x
+y = hero.pos.y
+# Move to all corners one by one and return to the start.
+hero.moveXY(x+gardenWidth, y)
+hero.moveXY(x+gardenWidth, y-gardenHeight)
+hero.moveXY(x, y-gardenHeight)
+hero.moveXY(x, y)
---
+++ Undefined/VolcanoFighters.py
@@ -0,0 +1,38 @@
+#https://codecombat.com/play/level/volcano-fighters
+# Complete the paladin rectangle to protect people.
+
+# The function find the most left unit.
+def findMostLeft (units):
+    if len(units) == 0:
+        return None
+    mostLeft = units[0]
+    for unit in units:
+        if unit.pos.x < mostLeft.pos.x:
+            mostLeft = unit
+    return mostLeft
+
+# The function find the most bottom unit.
+def findMostBottom (units):
+    if len(units) == 0:
+        return None
+    mostBottom = units[0]
+    for unit in units:
+        if unit.pos.y < mostBottom.pos.y:
+            mostBottom = unit
+    return mostBottom
+
+paladins = hero.findByType("paladin")
+# Find the top left paladin with findMostLeft function.
+ml = findMostLeft(paladins)
+# Find the bottom right paladin with findMostBottom function
+mb = findMostBottom(paladins)
+
+# Use X coordinate from the top left paladin
+# and Y coordinate from the bottom right paladin.
+x = ml.x
+y = mb.y
+# Move to {X, Y} point from the previous step.
+hero.moveXY(x, y)
+# Shield while the volcano is erupting.
+while True:
+    hero.shield()
