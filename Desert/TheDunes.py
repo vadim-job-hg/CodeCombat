d7f6c62e1f967c4f6a50f460bb23e9db21ecc382
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-11-24 21:44:39 +0200

    gameDev

---
+++ Desert/TheDunes.py
@@ -0,0 +1,23 @@
+#https://codecombat.com/play/level/the-dunes
+# Соберите монеты. Игнорируйте яков и бурлей. Сражайтесь с метателями и ограми.
+while True:
+    enemy = hero.findNearestEnemy()
+    item = hero.findNearestItem()
+    if enemy:
+        if enemy.type is "sand-yak" or enemy.type is "burl":
+            # Не сражайтесь с яками и бурлями! Просто продолжайте собирать монеты.
+            item = hero.findNearestItem()
+            if item:
+                hero.move(item.pos)
+            pass
+        else:
+            # Но если тип врага "thrower" или "ogre", атакуйте их.
+            if enemy.type is "thrower" or enemy.type is "ogre":
+                hero.attack(enemy)
+
+    elif item:
+        # Собирайте монеты
+        item = hero.findNearestItem()
+        if item:
+            hero.move(item.pos)
+        pass
