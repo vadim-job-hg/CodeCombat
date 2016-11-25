04c2d599d1665ce8e1679b5c4cbf8dce8674ac82
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-11-25 22:19:45 +0200

    gameDev

---
+++ Desert/StrandedInTheDunes.py
@@ -0,0 +1,26 @@
+#https://codecombat.com/play/level/stranded-in-the-dunes
+# Иди к крайней правой стороне карты, чтобы обнаружить новые места.
+# Смотри инструкцию для уточнения.
+def moveTo(position):
+    if (hero.isReady("jump")):
+        hero.jumpTo(position)
+    else:
+        hero.move(position)
+
+def attack(target):
+    if target:
+        if (hero.distanceTo(target) > 10):
+            moveTo(target.pos)
+        elif (hero.isReady("bash")):
+            hero.bash(target)
+        elif (hero.canCast('chain-lightning', target)):
+            hero.cast('chain-lightning', target)
+        else:
+            hero.attack(target)
+            hero.shield()
+while True:
+    enemy = hero.findNearestEnemy()
+    if enemy and enemy.type!='sand-yak':
+        attack(enemy)
+    else:
+        hero.move({'x':hero.pos.x+10, 'y':39})
