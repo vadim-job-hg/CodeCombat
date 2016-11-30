a57f1beb5543a0cc3aac76973fe13130c4c9c225
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-11-30 20:30:10 +0200

    subscr

---
+++ GameDev2/AdventureTime.py
@@ -0,0 +1,25 @@
+#https://codecombat.com/play/level/adventure-time
+# game.time is the time that has passed in the game
+game.spawnHeroXY("guardian", 10, 35)
+game.addSurviveGoal()
+game.addDefeatGoal(5)
+
+def onSpawn(event):
+    while True:
+        unit = event.target
+        enemy = unit.findNearestEnemy()
+        if enemy:
+            unit.attack(enemy)
+
+game.setActionFor("munchkin", "spawn", onSpawn)
+
+# game.time starts at zero, and counts upward in seconds
+spawnTime = 0
+while True:
+    # spawnTime is the time we want to spawn at
+    if game.time > spawnTime:
+        # Spawn a "munchkin" at 60, 35
+        game.spawnXY("munchkin", 35, 60)
+        # Set spawnTime equal to game.time plus 2
+        # so an enemy will spawn every 2 seconds.
+        spawnTime = game.time + 2
