1981b558c98af7bed5f2befbf9819ac28a9a0695
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-12-10 13:20:13 +0200

    subscr

---
+++ Mountain/MinedGems.py
@@ -0,0 +1,23 @@
+# https://codecombat.com/play/level/mined-gems
+
+# Collect 4 gems. Don't touch gems from the inner circle.
+
+# The radius of the gem-trap circle.
+innerRadius = hero.distanceTo(hero.findNearestItem())
+
+# This function check that a is definitely greater than b.
+def definitelyGreater(a, b):
+    return a > b + 0.5
+
+centre = {"x": 40, "y": 34}
+gems = hero.findItems()
+
+# Iterate all gems:
+for gem in gems:
+    # If the distance to the current gems is definitely
+    # greater than the inner radius:
+    if definitelyGreater(hero.distanceTo(gem), innerRadius):
+        # Collect the gem.
+        hero.moveXY(gem.pos.x, gem.pos.y)
+        # Return to the centre.
+        hero.moveXY(centre.x, centre.y)
---
+++ Mountain/SafeSpot.py
@@ -0,0 +1,36 @@
+# https://codecombat.com/play/level/safe-spot
+
+# Shout to activate the  bombs and move to the entrance.
+
+# The function checks if numbers are almost equal.
+def almostEqual(n1, n2):
+    return abs(n1 - n2) <= 0.5
+
+# The function checks that all
+# thangs are on the same distance from the hero.
+def allSameDistance(thangs):
+    if len(thangs) == 0:
+        return True
+    # We can use any thang as an etalon.
+    etalon = hero.distanceTo(thangs[0])
+    # Iterate all thangs:
+    for thang in thangs:
+        # Use almostEqual to check if not the distance
+        # between the unit and the etalon:
+        almost = almostEqual(hero.distanceTo(thang), etalon)
+        if not(almost):
+            # Return False.
+            return False
+    # All the same. Return  True.
+    return True
+
+bombs = hero.findEnemies()
+for x in range(36, 45):
+    for y in range(30, 39):
+        hero.moveXY(x, y)
+        if allSameDistance(bombs):
+            # It's a safe spot. Rock'n'Roll!
+            hero.say("HEEEEEEEEEY!!!")
+            hero.moveXY(40, 58)
+
+hero.say("Heh. Nothing.");
