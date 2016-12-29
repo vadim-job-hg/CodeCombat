83c56015dbc2e76db1bc1dfbd651e0682d8e646a
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2016-12-29 08:43:03 +0200

    Create AreaOfYetis.py

---
+++ Mountain/AreaOfYetis.py
@@ -0,0 +1,18 @@
+#https://codecombat.com/play/level/area-of-yetis
+# Defeat Yetis.
+
+# The chosen one can stun yetis with the Shout.
+chosen = hero.findFriends()[0]
+
+# The power of the Shout is equal to the area of a circle.
+radius = chosen.distanceTo(chosen.findNearestEnemy())
+# The area of a circle can be calculated with the formula:
+area = radius * radius * Math.PI
+# Tell the area to the chosen one.
+hero.say(area)
+# Don't give up! Fight!
+while True:
+    enemyattack = hero.findNearestEnemy()
+    if (enemyattack):
+        hero.attack(enemyattack)
+
