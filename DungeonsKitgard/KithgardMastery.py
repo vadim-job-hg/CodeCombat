7f9cd07e1cd6b0c06abebddce5d22314c73e4d3b
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2017-01-14 21:23:34 +0200

    Update KithgardMastery.py

--- DungeonsKitgard/KithgardMastery.py
+++ DungeonsKitgard/KithgardMastery.py
@@ -1,26 +1,25 @@
-i = 1;
-while True:
+# Достигни конца лабиринта, используя команды перемещения.
+# Подсчитай количество подобранных самоцветов и скажи это количество рядом с огненной ловушкой, чтобы отключить её.
+# Ворон на старте выдаст тебе пароль. Скажи пароль рядом с дверью, чтобы открыть её.
+# Убей огров, рядом с которыми окажешься.
+# Ты можешь использовать цикл 'loop' для повтора инструкций, если необходимо.
+# Если ты пройдёшь этот уровень, то сможешь перейти в Лес Темнодрев!
+i = 0
+while True:    
+    self.moveUp()
+    self.moveRight(3)
+    self.moveUp()
+    i = i + 1
+    self.moveDown()
     self.moveRight()
+    self.say('Swordfish')
+    self.moveRight(2)
     self.moveUp()
-    self.moveUp()
+    hero.say(i)
+    self.moveUp(2)
     enemy = self.findNearestEnemy()
     if enemy:
         self.attack(enemy)
-        self.attack(enemy)
-    self.moveLeft()
-    self.moveUp()
-    self.moveUp()
-    self.moveRight()
-    self.moveRight()
-    self.moveUp()
-    self.moveDown()
-    self.moveRight()
-    self.moveDown()
-    self.moveDown()
-    self.say(i)
-    self.moveDown()
-    self.moveDown()
-    self.say('Sesame')
-    self.moveRight()
-    self.moveRight()
-    i = i +1
+    self.moveLeft(4)
+    self.moveUp(2)
+    
