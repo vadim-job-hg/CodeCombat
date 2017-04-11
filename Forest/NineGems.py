d5c6d147b65e527ee3f5fd88b48ca23662192b65
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2017-04-11 10:39:36 +0300

    #nine-gems

---
+++ Forest/NineGems.py
@@ -0,0 +1,8 @@
+# https://codecombat.com/play/level/nine-gems
+
+# Collect all the gems in 4 moveXY's or less!
+# Programmers need to think creatively!
+hero.moveXY(32, 48)
+hero.moveXY(70, 48)
+hero.moveXY(31, 10)
+hero.moveXY(32, 37)
