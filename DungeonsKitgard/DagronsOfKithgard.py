379469a6c48baf79dbdc7eb740067d1a580c9ef2
vadim-job-hg <vadim-job-hg@yandex.ru>
2017-05-14 11:34:18 +0300

    subscr

---
+++ DungeonsKitgard/DagronsOfKithgard.py
@@ -0,0 +1,8 @@
+# https://codecombat.com/play/level/dagrons-of-kithgard
+# Иди к самоцвету.
+# Не касайся стен!
+# Напиши свой код ниже и нажми "ЗАПУСТИТЬ", когда закончишь.
+
+hero.moveRight()
+hero.moveDown()
+hero.moveRight()
