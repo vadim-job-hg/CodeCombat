70a752f58111c46685509e011a130da58e9ec587
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2017-05-25 13:22:48 +0300

    Create Airdrop.py

---
+++ Forest/Airdrop.py
@@ -0,0 +1,19 @@
+# https://codecombat.com/play/level/airdrop
+# Get all swords and protect the village.
+
+def onSpawn (event):
+    while True:
+        item = hero.findNearestItem()
+        if item:
+            pet.fetch(item)
+        
+
+# Assign onSpawn function for the pet's "spawn".
+pet.on("spawn", onSpawn)
+
+while True:
+    # Guard the left passage: 
+    enemy = hero.findNearestEnemy()
+    if enemy:
+        hero.attack(enemy)
+    
