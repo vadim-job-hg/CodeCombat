78d921829a75d847ca0906fa0d3b816025308d1e
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2017-05-28 12:24:48 +0300

    Update PrimePathing.py

--- Glacier/PrimePathing.py
+++ Glacier/PrimePathing.py
@@ -1,60 +1,47 @@
 # Get all your troops to the end of the path by passing over the mines.
 # You can locate duds by finding the mines that have a value that is a prime number.
 # Check the guide for clarification.
-route = [[6, 41, 0], [3, 16, 1], [79, 5, 0], [91, 12, 1], [82, 62, 0], [81, 62, 0], [80, 62, 0], [79, 62, 0],
-         [78, 62, 0], [78, 62, 0], [77, 62, 0]]
-routeIndex = 0
-SafeWay = [[70, 40]]
-SafeWayIndex = 1
 
-
-def moveTroop():
-    moved = True
-    for index, friend in enumerate(hero.findFriends()):
-        ind = SafeWayIndex - index
-        if ind >= 0 and ind < len(SafeWay):
-            if friend.pos.x != SafeWay[ind][0] or friend.pos.y != SafeWay[ind][1]:
-                moved = False
-                hero.command(friend, 'move', {'x': SafeWay[ind][0], 'y': SafeWay[ind][1]})
-    return moved
-
-
-def checkprimary(value):
-    for j in range(2, Math.ceil(Math.sqrt(value))):
-        if value % j == 0:
+def primeCheck(number):
+    for i in range(2,(number**0.5)):
+        if number % i == 0:
             return False
     return True
 
+friends = hero.findFriends()
+mines = hero.findHazards()
+firstMines = []
+for mine in mines:
+    if mine.pos.y > 30 and mine.pos.x < 80:
+        if primeCheck(mine.value):
+            firstMines.append(mine)
+for mine in firstMines:
+    if mine.pos.x > (hero.pos.x - 25):
+        hero.moveXY(mine.pos.x + 10, mine.pos.y)
+        hero.moveXY(mine.pos.x, mine.pos.y)
+        hero.moveXY(hero.pos.x - 10, hero.pos.y)
+        for friend in friends:
+            hero.command(friend, "move", {"x": mine.pos.x + 10, "y": mine.pos.y})
+            hero.wait(1.6)
+            hero.command(friend, "move", {"x": mine.pos.x, "y": mine.pos.y})
+            hero.command(friend, "move", {"x": mine.pos.x - 10, "y": mine.pos.y})
+hero.moveXY(2, 15)
+for friend in friends:
+    hero.command(friend, "move", {"x": 2, "y": 15})
 
-def checkifExist(object):
-    for wayEl in SafeWay:
-        if object.pos.x == wayEl[0] and object.pos.y == wayEl[1]:
-            return False
-    return True
-
-
-def checktrap():
-    objects = hero.findHazards()
-    for object in objects:
-        if (hero.distanceTo(object) < 20 and checkifExist(object) and checkprimary(object.value)):
-            hero.moveXY(object.pos.x, object.pos.y)
-            SafeWay.push([object.pos.x, object.pos.y])
-            return True
-    return False
-
-
-while True:
-    checktrap()
-    if routeIndex < len(route):
-        if route[routeIndex][2] == 1:
-            hero.moveXY(route[routeIndex][0], route[routeIndex][1])
-            SafeWay.push(route[routeIndex])
-            routeIndex = routeIndex + 1
-        else:
-            hero.move({'x': route[routeIndex][0], 'y': route[routeIndex][1]})
-            if hero.pos.x == route[routeIndex][0] and hero.pos.y == route[routeIndex][1]:
-                SafeWay.push(route[routeIndex])
-                routeIndex = routeIndex + 1
-
-    if moveTroop():
-        SafeWayIndex = SafeWayIndex + 1
+secondMines = []
+for mine in mines:
+    if mine.pos.y < 30 and mine.pos.x < 80:
+        if primeCheck(mine.value):
+            secondMines.append(mine)
+while hero.pos.x < 75:
+    for mine in secondMines:
+        if mine.pos.x < (hero.pos.x + 25) and mine.pos.x > hero.pos.x:
+            hero.moveXY(mine.pos.x - 10, mine.pos.y)
+            hero.moveXY(mine.pos.x, mine.pos.y)
+            hero.moveXY(hero.pos.x + 10, hero.pos.y)
+            for friend in friends:
+                hero.command(friend, "move", {"x": mine.pos.x - 5, "y": mine.pos.y})
+                hero.wait(1.6)
+                hero.command(friend, "move", {"x": mine.pos.x, "y": mine.pos.y})
+                hero.command(friend, "move", {"x": mine.pos.x + 12, "y": mine.pos.y})
