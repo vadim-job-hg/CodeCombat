06b9b47d9f329c7b805d683fba4d45141c75b6e7
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2017-06-04 00:01:17 +0300

    Create UndergroundBusiness.py

---
+++ Desert/UndergroundBusiness.py
@@ -0,0 +1,26 @@
+https://codecombat.com/play/level/underground-business
+# Accumulate 300 gold and escape from the dungeon.
+
+def onSpawn(event):
+    # Send the pet to walk around the dungeon:
+    pet.moveXY(26, 55)
+    pet.moveXY(71, 57)
+    pet.moveXY(71, 11)
+    pet.moveXY(20, 11)
+    pet.moveXY(21, 36)
+    pet.moveXY(hero.pos.x, hero.pos.y)
+    # Don't forget to return it to the hero:
+    
+    pass
+
+pet.on("spawn", onSpawn)
+
+while True:
+    # Guard peasants:
+    enemy = hero.findNearestEnemy()
+    # When you have 300+ gold move to the red mark:
+    if enemy:
+        hero.attack(enemy)
+    if hero.gold>=300:
+        break
+hero.moveXY(50, 34)
