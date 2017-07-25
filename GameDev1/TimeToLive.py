41fd09e6560b5c151f3e0e91d3bce2bb9eaedd8e
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2017-07-25 17:17:51 +0300

    Update TimeToLive.py

--- GameDev1/TimeToLive.py
+++ GameDev1/TimeToLive.py
@@ -1,17 +1,22 @@
 # https://codecombat.com/play/level/time-to-live
 
-# Generators spawn enemies over time.
-# Skeletons are afraid of lightstones.
+# Pass an argument to addSurviveGoal() to specify a time.
 
-player = game.spawnHeroXY("champion", 15, 35)
-player.attackDamage = 60
-player.maxSpeed = 8
 
-game.addSurviveGoal()
-game.addDefeatGoal()
+# This means the player must survive for 20 seconds.
+game.addSurviveGoal(20)
 
-# Spawn a "generator"
-game.spawnXY("generator", 20, 25)
-# Spawn a "lightstone"
-game.spawnXY("lightstone", 20, 25)
-# Now beat your game!
+# Spawn a generator with spawnXY
+# Remember to assign the spawned generator to a variable!
+# Use the variable to configure the generator below.
+game.spawnXY("generator", 7, 10)
+# Set the generator's spawnType to "munchkin"
+game.spawnType ="munchkin"
+# Use spawnHeroXY to spawn a hero for the player.
+# Remember to assign the spawned hero to a variable!
+hero = game.spawnHeroXY("captain",69, 58)
+# Set the hero's maxHealth to 100
+hero.maxHealth = 100
+# Set the hero's attackDamage to 10
+hero.attackDamage = 10
+# Play the game!
