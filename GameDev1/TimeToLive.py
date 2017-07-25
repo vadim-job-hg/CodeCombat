9d46392cd80f2c7f67c08345260a49e035bb4231
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2017-07-25 17:14:05 +0300

    Create TimeToLive.py

---
+++ GameDev1/TimeToLive.py
@@ -0,0 +1,17 @@
+# https://codecombat.com/play/level/time-to-live
+
+# Generators spawn enemies over time.
+# Skeletons are afraid of lightstones.
+
+player = game.spawnHeroXY("champion", 15, 35)
+player.attackDamage = 60
+player.maxSpeed = 8
+
+game.addSurviveGoal()
+game.addDefeatGoal()
+
+# Spawn a "generator"
+game.spawnXY("generator", 20, 25)
+# Spawn a "lightstone"
+game.spawnXY("lightstone", 20, 25)
+# Now beat your game!
