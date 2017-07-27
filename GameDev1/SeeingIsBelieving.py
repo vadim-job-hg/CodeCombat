50e73f11285361ff5aa26cb07431544754abc2d1
Винямин Карлович Штольц <info@knubiosoft.com>
2017-07-27 11:25:50 +0300

    #####

--- GameDev1/SeeingIsBelieving.py
+++ GameDev1/SeeingIsBelieving.py
@@ -1,8 +1,7 @@
 # https://codecombat.com/play/level/seeing-is-believing
-# todo:
 # Игроки любят видеть результат, используй `ui.track()`!
 # Таким образом ты создашь элемент для отображения.
-hero = game.spawnHeroXY(20, 20, "samurai")
+hero = game.spawnHeroXY(45, 45, "samurai")
 
 spawner = game.spawnXY("generator", 50, 50)
 spawner.spawnType = "munchkin"
@@ -15,9 +14,11 @@ game.addSurviveGoal(20)
 # Используй `ui.track`, чтобы отслеживать свойство "defeated":
 ui.track(game, "defeated")
 # Измени у героя `attack` и `maxSpeed`:
-hero.attack = 20000
+hero.attackDamage = 20000
 hero.maxSpeed = 50
 # Добавь на поле боя точки создания врагов:
-spawner2 = game.spawnXY("generator", 50, 50)
+spawner2 = game.spawnXY("generator", 43, 56)
 spawner2.spawnType = "munchkin"
 # Начни играть и победи 10 манчкинов или скелетов!
+spawner2 = game.spawnXY("generator", 37, 43)
+spawner2.spawnType = "munchkin"
