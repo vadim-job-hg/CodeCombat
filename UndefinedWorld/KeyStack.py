12c34c64dc1e552698781dbcb4b24e045b2cca35
Винямин Карлович Штольц <info@knubiosoft.com>
2017-08-01 17:20:53 +0300

    #####

---
+++ UndefinedWorld/KeyStack.py
@@ -0,0 +1,25 @@
+# https://codecombat.com/play/level/key-stack
+# Open doors and collect treasures.
+
+peasant = hero.findByType("peasant")[0]
+
+goldKey = peasant.findByType("gold-key")[0]
+silverKey = peasant.findByType("silver-key")[0]
+bronzeKey = peasant.findByType("bronze-key")[0]
+
+# Command the peasant to pick up the gold and silver keys.
+hero.command(peasant, "pickUpItem", goldKey)
+hero.command(peasant, "pickUpItem", silverKey)
+# Now, command the peasant to pick up the last key:
+hero.command(peasant, "pickUpItem", bronzeKey)
+
+# Command the peasant to drop a key near the first door.
+hero.command(peasant, "dropItem", {"x": 40, "y": 34})
+# The second key -> the second door.
+hero.command(peasant, "dropItem", {"x": 31, "y": 34})
+# Drop the first (in the stack) key to the last door:
+hero.command(peasant, "dropItem", {"x": 21, "y": 34})
+hero.command(peasant, "move", {"x": 9, "y": 41})
+# Hurry and collect treasures!
+hero.moveXY(11, 59)
+hero.moveXY(11, 11)
