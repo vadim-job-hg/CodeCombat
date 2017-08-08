2ea2a6561639f750c8ace881304035f7e1dcf97a
Винямин Карлович Штольц <info@knubiosoft.com>
2017-08-08 10:41:26 +0300

    #####

--- Glacier/KeyStack.py
+++ Glacier/KeyStack.py
@@ -19,7 +19,18 @@ hero.command(peasant, "dropItem", {"x": 40, "y": 34})
 hero.command(peasant, "dropItem", {"x": 31, "y": 34})
 # Drop the first (in the stack) key to the last door:
 hero.command(peasant, "dropItem", {"x": 21, "y": 34})
-hero.command(peasant, "move", {"x": 9, "y": 41})
+#hero.command(peasant, "move", {"x": 9, "y": 41})
 # Hurry and collect treasures!
-hero.moveXY(11, 59)
-hero.moveXY(11, 11)
+hero.moveXY(14, 34)
+hero.jumpTo(Vector(14, 50))
+hero.moveXY(11, 58)
+hero.moveXY(14, 44)
+while not(hero.isReady('jump')):
+    hero.wait()
+hero.jumpTo(Vector(14, 34))
+hero.say('wooh')
+hero.moveXY(14, 34)
+while not(hero.isReady('jump')):
+    hero.wait()
+hero.jumpTo(Vector(14, 22))
+hero.moveXY(10, 11)
