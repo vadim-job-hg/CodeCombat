9e2224512da2809390f90da4d74212f5507ed437
vadim-job-hg <vadim-job-hg@yandex.ru>
2017-08-27 14:04:03 +0300

    :alien:

---
+++ Glacier/GemByGem.py
@@ -0,0 +1,21 @@
+# https://codecombat.com/play/level/gem-by-gem
+# Connect gems to a linked list to open the door.
+
+# Use that gems a base for the list.
+baseGem = hero.findByType("root-gem")[0]
+# Those gems are not linked yet.
+freeGems = hero.findByType("gem")
+
+# Use the property "next" to link elements to the list.
+# Append an element before the base gem.
+freeGems[0].next = baseGem
+# And one more at the new head of the list:
+freeGems[1].next = freeGems[0]
+# Next add more gems at the end.
+baseGem.next = freeGems[2]
+# Add two more gems one by one:
+freeGems[2].next = freeGems[3]
+freeGems[3].next = freeGems[4]
+while True:
+    if hero.isPathClear(hero.pos, {"x": 76, "y": 34}):
+        hero.moveXY(76, 34)
