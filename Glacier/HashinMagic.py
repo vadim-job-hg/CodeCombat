3948d3f31526ca92b7a317800316860c5eb9cd8f
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2017-09-12 16:08:17 +0300

    Create HashinMagic.py

---
+++ Glacier/HashinMagic.py
@@ -0,0 +1,20 @@
+# https://codecombat.com/play/level/hashing-magic
+# Use the hash function to find places for soldiers.
+totalCells = 135
+
+# This function hashes a string value to an integer.
+def hashName(name):
+    hash = 0
+    for i in range(0, len(name)):
+        hash += ord(name[i])
+    # The hash value should be from 0 to 134.
+    # Use modulus operation to "cut" the hash value.
+    while hash>=totalCells:
+        hash -= totalCells
+    return hash
+
+soldiers = hero.findByType("soldier")
+# For each soldier say his/her name (id) and hash of it.
+# The name and the number should be splitted by whitespace.
+for soldier in soldiers:
+    hero.say(soldier.id +' '+hashName(soldier.id))
