858c06e7b34556abe705c72910e10993b90468cc
vadim-job-hg <vadim-job-hg@yandex.ru>
2017-09-23 22:51:23 +0300

    :alien:

--- Mountain/BlackDiamond.py
+++ Mountain/BlackDiamond.py
@@ -4,12 +4,14 @@ while True:
     gem = hero.findNearestItem()
     if gem:
         clear = hero.isPathClear(hero.pos, gem.pos)
+    else:
+        clear = False
     # Метод isPathClear моказывает наличие препятствий на пути.
     # Если все чисто, иди к камню. Используй move() и gem.pos.
     if clear:
         lastpos = {'x': hero.pos.x, 'y': hero.pos.y}
         hero.moveXY(gem.pos.x, gem.pos.y)
-        else:
+    else:
         hero.moveXY(lastpos.x, lastpos.y)
         hero.moveXY(40, 35)
     # В противном случае возвращайся в центр.
