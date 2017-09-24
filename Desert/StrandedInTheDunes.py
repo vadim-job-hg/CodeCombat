7222467a2bc856b24fa21e89a4d6b04094352dec
vadim-job-hg <vadim-job-hg@yandex.ru>
2017-09-24 11:46:50 +0300

    :alien:

--- Desert/StrandedInTheDunes.py
+++ Desert/StrandedInTheDunes.py
@@ -1,16 +1,12 @@
+# Двигайся к крайней правой стороне карты, чтобы обнаружить новые места.
+# Если нужно больше информации, загляни в "Советы".
 # https://codecombat.com/play/level/stranded-in-the-dunes
 # Иди к крайней правой стороне карты, чтобы обнаружить новые места.
 # Смотри инструкцию для уточнения.
-def moveTo(position):
-    if (hero.isReady("jump")):
-        hero.jumpTo(position)
-    else:
-        hero.move(position)
-
 def attack(target):
-    if target:
+    if target  and target.type!='sand-yak':
         if (hero.distanceTo(target) > 10):
-            moveTo(target.pos)
+            hero.move(target.pos)
         elif (hero.isReady("bash")):
             hero.bash(target)
         elif (hero.canCast('chain-lightning', target)):
@@ -18,9 +14,21 @@ def attack(target):
         else:
             hero.attack(target)
             hero.shield()
-while True:
+#step 1
+hero.moveXY(120,39)
+while hero.time<100:
     enemy = hero.findNearestEnemy()
     if enemy and enemy.type!='sand-yak':
         attack(enemy)
     else:
-        hero.move({'x':hero.pos.x+10, 'y':39})
+        hero.move({'x':120, 'y':39})
+#step 2
+while True:
+    enemy = hero.findNearestEnemy()
+    item = hero.findNearestItem()
+    if item:
+        hero.move(item.pos)
+    elif enemy:
+        attack(enemy)
+    else:
+        hero.move({'x':60, 'y':35})
