26b8f66461d2be26d4be9dda8cb390492990aa1c
Vadim Kuznetsov <vadimjobhg86@gmail.com>
2017-10-12 10:58:37 +0300

    :alien:

---
+++ GameDev2/GameOfCoinsiteration1.py
@@ -0,0 +1,89 @@
+# https://codecombat.com/play/level/game-of-coins-iteration-1?
+# Build a forest, coin, and mushroom grid.
+
+# First we need the forest.
+# Let's put those tiles as a grid.
+game.spawnXY("forest", 16, 16)
+game.spawnXY("forest", 32, 16)
+game.spawnXY("forest", 48, 16)
+game.spawnXY("forest", 64, 16)
+game.spawnXY("forest", 16, 32)
+game.spawnXY("forest", 32, 32)
+game.spawnXY("forest", 48, 32)
+game.spawnXY("forest", 64, 32)
+game.spawnXY("forest", 16, 48)
+# As you can see the distance between them is 16.
+# Place more forest tiles in the three spots
+# where they are missing
+game.spawnXY("forest", 64, 48)
+game.spawnXY("forest", 32, 48)
+game.spawnXY("forest", 48, 48)
+# Next we're placing coins in free space.
+# Leave some free space for mushrooms.
+# Scroll down, there are many coins.
+game.spawnXY("bronze-coin", 16, 8)
+game.spawnXY("bronze-coin", 24, 8)
+game.spawnXY("bronze-coin", 32, 8)
+game.spawnXY("bronze-coin", 48, 8)
+game.spawnXY("bronze-coin", 56, 8)
+game.spawnXY("bronze-coin", 64, 8)
+game.spawnXY("bronze-coin", 72, 8)
+
+game.spawnXY("bronze-coin", 8, 16)
+game.spawnXY("bronze-coin", 24, 16)
+game.spawnXY("bronze-coin", 40, 16)
+game.spawnXY("bronze-coin", 56, 16)
+game.spawnXY("bronze-coin", 72, 16)
+
+game.spawnXY("bronze-coin", 8, 24)
+game.spawnXY("bronze-coin", 16, 24)
+game.spawnXY("bronze-coin", 24, 24)
+game.spawnXY("bronze-coin", 32, 24)
+game.spawnXY("bronze-coin", 40, 24)
+game.spawnXY("bronze-coin", 48, 24)
+game.spawnXY("bronze-coin", 56, 24)
+game.spawnXY("bronze-coin", 64, 24)
+game.spawnXY("bronze-coin", 72, 24)
+
+game.spawnXY("bronze-coin", 24, 32)
+game.spawnXY("bronze-coin", 56, 32)
+
+game.spawnXY("bronze-coin", 8, 40)
+game.spawnXY("bronze-coin", 16, 40)
+game.spawnXY("bronze-coin", 24, 40)
+game.spawnXY("bronze-coin", 32, 40)
+game.spawnXY("bronze-coin", 40, 40)
+game.spawnXY("bronze-coin", 48, 40)
+game.spawnXY("bronze-coin", 56, 40)
+game.spawnXY("bronze-coin", 64, 40)
+game.spawnXY("bronze-coin", 72, 40)
+
+game.spawnXY("bronze-coin", 8, 48)
+game.spawnXY("bronze-coin", 24, 48)
+game.spawnXY("bronze-coin", 40, 48)
+game.spawnXY("bronze-coin", 56, 48)
+game.spawnXY("bronze-coin", 72, 48)
+
+game.spawnXY("bronze-coin", 8, 56)
+game.spawnXY("bronze-coin", 16, 56)
+game.spawnXY("bronze-coin", 24, 56)
+game.spawnXY("bronze-coin", 32, 56)
+
+# The distance is 8. Place more coins here.
+game.spawnXY("bronze-coin", 48, 56)
+game.spawnXY("bronze-coin", 56, 56)
+game.spawnXY("bronze-coin", 64, 56)
+game.spawnXY("bronze-coin", 72, 56)
+# Mushrooms will be our powerups.
+game.spawnXY("mushroom", 40, 8)
+game.spawnXY("mushroom", 8, 32)
+game.spawnXY("mushroom", 40, 56)
+game.spawnXY("mushroom", 72, 32)
+# Place more mushrooms.
+
+
+# The hero and goal setup.
+hero = game.spawnHeroXY('knight', 8, 8)
+hero.maxSpeed = 30
+game.addCollectGoal()
+# Test what you’ve done to make sure it works
