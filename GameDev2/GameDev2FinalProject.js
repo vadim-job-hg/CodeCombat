7a109aae231ff71f554dd1de68db80656a99c3f5
Vadim Kuznetsov <vadimjobhg86@gmail.com>
2017-10-17 11:43:30 +0300

    :alien:

---
+++ GameDev2/GameDev2FinalProject.js
@@ -0,0 +1,30 @@
+// https://codecombat.com/play/level/game-dev-2-final-project?
+// Create your own game!
+
+// Spawn a hero with spawnHeroXY()
+var hero = game.spawnHeroXY(12, 10);
+// Add at least one goal!
+game.addDefeatGoal();
+game.addSurviveGoal();
+// Spawn objects into the game with spawnXY()
+// Spawn 2 "soldier"s.
+game.spawnXY("soldier", 35, 20);
+game.spawnXY("soldier", 45, 20);
+// Spawn 2 "archer"s.
+game.spawnXY("archer", 35, 20);
+game.spawnXY("archer", 45, 20);
+// Spawn 2 "soldier"s.
+game.spawnXY("soldier", 35, 20);
+game.spawnXY("soldier", 45, 20);
+// Spawn 2 "archer"s.
+game.spawnXY("archer", 35, 20);
+game.spawnXY("archer", 45, 20);
+// Spawn 2 "soldier"s.
+game.spawnXY("soldier", 35, 20);
+game.spawnXY("soldier", 45, 20);
+// Spawn 2 "archer"s.
+game.spawnXY("archer", 35, 20);
+game.spawnXY("archer", 45, 20);
+var munchkin1 = game.spawnXY("munchkin", 25, 30);
+var munchkin2 = game.spawnXY("munchkin", 25, 35);
+var munchkin3 = game.spawnXY("munchkin", 25, 40);
