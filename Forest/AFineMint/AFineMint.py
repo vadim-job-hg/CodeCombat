0b35c861e4b76b8c54de6bf493ac11a42a7abf61
Vadim Kuznetsov <vadimjobhg86@gmail.com>
2017-12-28 10:40:36 +0200

    :grinning:

--- Forest/AFineMint/AFineMint.py
+++ Forest/AFineMint/AFineMint.py
@@ -1,23 +1,13 @@
-# http://codecombat.com/play/level/a-fine-mint
-# Peons are trying to steal your coins!
-# Write a function to squash them before they can take your coins.
-
 def pickUpCoin():
-    # coin = hero.findNearestItem()
     coin = hero.findNearestItem()
     if coin:
         hero.moveXY(coin.pos.x, coin.pos.y)
 
-
-# Write the attackEnemy function below.
-# Find the nearest enemy and attack them if they exist!
 def attackEnemy():
-    # enemy = hero.findNearestEnemy()
     enemy = hero.findNearestEnemy()
     if enemy:
         hero.attack(enemy)
 
-
 while True:
-    attackEnemy()  # ∆ Uncomment this line after you write an attackEnemy function.
+    attackEnemy()
     pickUpCoin()
