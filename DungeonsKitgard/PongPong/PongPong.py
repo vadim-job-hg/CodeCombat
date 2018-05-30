37f0132c7d0943165a463fc9507d6e91d6e2bf15
Vadim Kuznetsov <vadimjobhg86@gmail.com>
2018-05-30 10:00:57 +0300

    :grinning:

--- DungeonsKitgard/PongPong/PongPong.py
+++ DungeonsKitgard/PongPong/PongPong.py
@@ -1,6 +1,4 @@
-i = 1
-while True:
+for i in [3, -3, 3, -3, 3, -3, 3, -3, 3, -3, 3, -3]:
     hero.attack('ball')
     hero.attack('ball2')
-    hero.moveRight(3*i)
-    i *= -1
+    hero.moveRight(i)
