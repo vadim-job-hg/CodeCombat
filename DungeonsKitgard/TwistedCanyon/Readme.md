8a6374950ad273bf285e9b5b5f467de56e0ef6ee
Vadim Kuznetsov <vadimjobhg86@gmail.com>
2018-06-05 10:07:18 +0300

    :grinning:

---
+++ DungeonsKitgard/TwistedCanyon/Readme.md
@@ -0,0 +1,20 @@
+# TrueNames 
+
+https://codecombat.com/play/level/twisted-canyon?
+<table>
+<tr>
+<td>
+
+![Hero Picture](hero.png?raw=true "Hero Picture")
+
+</td>
+<td>
+<ul>
+<li>
+
+[PYTHON](TwistedCanyon.py)
+
+</li>
+</td>
+</tr>
+<table>
---
+++ DungeonsKitgard/TwistedCanyon/TwistedCanyon.py
@@ -0,0 +1,20 @@
+# Escape from the maze by moving to the X mark.
+# Try to collect as many coins as you can.
+hero.attack("Treasure Chest")
+hero.moveUp()
+hero.moveDown()
+hero.attack("Wicket")
+hero.moveRight()
+hero.moveUp(2)
+hero.moveDown(2)
+hero.moveLeft(3)
+while True:
+    hero.moveUp(2)
+    enemy = hero.findNearestEnemy()
+    hero.attack(enemy)
+    hero.moveLeft(2)
+    hero.moveRight(2)
+    hero.moveDown()
+    hero.moveRight()
+    hero.moveUp()
+    hero.moveRight()
