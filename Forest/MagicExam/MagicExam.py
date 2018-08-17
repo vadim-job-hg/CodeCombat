70cc4993202746c60b60a648327666bc58324cd5
User <vadim-job-hg@yandex.ru>
2018-08-17 20:58:31 +0300

    Merge branch 'master' of github.com:vadim-job-hg/CodeCombat
    # Conflicts:
    #	Functions/Necromancer.py
    #	Functions/Wizard.py

--- Forest/MagicExam/MagicExam.py
+++ Forest/MagicExam/MagicExam.py
@@ -1,14 +1,40 @@
 # Try to get the best grade (gold) at the magic exam.
 # Move to each X mark, then use a spell.
-hero.moveXY(18, 40)
-friend = hero.findNearestFriend()
-hero.cast('heal', friend)
+def action():
+    friend = hero.findNearestFriend()
+    if friend:
+        if friend.type == 'soldier':
+            hero.cast('heal', friend)
+        elif friend.type == 'goliath':
+            hero.cast('grow', friend)
+        else:
+            hero.cast('regen', friend)
+    enemy = hero.findNearestEnemy()
+    if enemy:
+        if enemy.type == 'ogre':
+            hero.cast('force-bolt', enemy)
+        elif enemy.type == 'brawler':
+            hero.cast('shrink', enemy)
+        else:
+            hero.cast('poison-cloud', enemy)
+
+
 hero.moveXY(18, 24)
-enemy = hero.findNearestEnemy()
-hero.cast('force-bolt', enemy)
+action()
+hero.moveXY(18, 40)
+action()
 hero.moveXY(34, 24)
-enemy = hero.findNearestEnemy()
-hero.cast('shrink', enemy)
+action()
 hero.moveXY(34, 40)
-friend = hero.findNearestFriend()
-hero.cast('grow', friend)
+action()
+hero.moveXY(50, 40)
+action()
+hero.moveXY(50, 24)
+action()
+hero.moveXY(66, 40)
+item = hero.findNearestItem()
+hero.cast('grow', hero)
+hero.moveXY(item.pos.x, item.pos.y)
+hero.moveXY(66, 24)
+item = hero.findNearestItem()
+hero.moveXY(item.pos.x, item.pos.y)
