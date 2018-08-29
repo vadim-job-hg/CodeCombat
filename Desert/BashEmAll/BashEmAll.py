00c7651a929ee2d699c9e055b6a6f363983bfc7a
mra1984 <42181286+mra1984@users.noreply.github.com>
2018-08-29 16:04:34 +0100

    Update BashEmAll.py

--- Desert/BashEmAll/BashEmAll.py
+++ Desert/BashEmAll/BashEmAll.py
@@ -10,3 +10,18 @@ while True:
         if item:
             hero.moveXY(item.pos.x, item.pos.y)
             hero.moveXY(40, 34)
+
+# I found this works better.  With your method my hero could not bash far enough using 'Anya' 
+while True:
+    enemy = hero.findNearestEnemy()
+    if enemy:
+        if hero.isReady("bash"):
+            hero.bash(enemy)
+        else:
+            hero.shield()
+    else:
+        gem = hero.findNearestItem()
+        if gem:
+            hero.moveXY(gem.pos.x, gem.pos.y)
+        else:
+            hero.moveXY(40, 34)
