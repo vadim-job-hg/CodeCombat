15903c2e9f6e41b1326dbf3147cf54ad06dc91a3
Vadim Kuznetsov <vadimjobhg86@gmail.com>
2018-09-13 10:48:02 +0300

    Adding placeholder pictures

--- Desert/BashEmAll/BashEmAll.py
+++ Desert/BashEmAll/BashEmAll.py
@@ -4,10 +4,12 @@ while True:
         if hero.isReady("bash"):
             hero.bash(enemy)
         else:
+            hero.moveXY(40, 34)
             hero.shield()
     else:
         gem = hero.findNearestItem()
         if gem:
             hero.moveXY(gem.pos.x, gem.pos.y)
+            hero.moveXY(40, 34)
         else:
             hero.moveXY(40, 34)
