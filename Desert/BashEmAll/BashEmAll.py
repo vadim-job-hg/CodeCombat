1c16c2dd2fe669770bf153c19a400c916b2e6481
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2018-09-13 10:40:22 +0300

    Update BashEmAll.py

--- Desert/BashEmAll/BashEmAll.py
+++ Desert/BashEmAll/BashEmAll.py
@@ -1,20 +1,6 @@
 while True:
     enemy = hero.findNearestEnemy()
     if enemy:
-        if (hero.isReady('bash')):
-            hero.bash(enemy)
-        else:
-            hero.moveXY(40, 34)
-    else:
-        item = hero.findNearestItem()
-        if item:
-            hero.moveXY(item.pos.x, item.pos.y)
-            hero.moveXY(40, 34)
-
-# I found this works better.  With your method my hero could not bash far enough using 'Anya' 
-while True:
-    enemy = hero.findNearestEnemy()
-    if enemy:
         if hero.isReady("bash"):
             hero.bash(enemy)
         else:
