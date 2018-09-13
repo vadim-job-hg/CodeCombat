9b47457a59470649ae48802b45d0109372e6b432
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2018-09-13 10:43:47 +0300

    Update GemByGem.py

--- Glacier/GemByGem/GemByGem.py
+++ Glacier/GemByGem/GemByGem.py
@@ -1,4 +1,3 @@
-# https://codecombat.com/play/level/gem-by-gem
 # Connect gems to a linked list to open the door.
 
 # Use that gems a base for the list.
@@ -10,12 +9,12 @@ freeGems = hero.findByType("gem")
 # Append an element before the base gem.
 freeGems[0].next = baseGem
 # And one more at the new head of the list:
-
+freeGems[1].next = freeGems[0]
 # Next add more gems at the end.
 baseGem.next = freeGems[2]
 # Add two more gems one by one:
-
-
+freeGems[2].next = freeGems[3]
+freeGems[3].next = freeGems[4]
 while True:
     if hero.isPathClear(hero.pos, {"x": 76, "y": 34}):
         hero.moveXY(76, 34)
