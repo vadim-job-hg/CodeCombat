7f89397c1bdad57154c7767fae6fae32095c7b1d
Vadim Kuznetsov <vadimjobhg86@gmail.com>
2018-12-26 14:23:23 +0200

    Some fixes

--- Desert/TeamWork/TeamWork.py
+++ Desert/TeamWork/TeamWork.py
@@ -1,17 +1,19 @@
-# https://codecombat.com/play/level/team-work
-# Should fill in some default source
-def commandTroops():
-    for index, friend in enumerate(hero.findFriends()):
-        if friend.type == 'peasant':
-            CommandPeasant(friend)
+# Самоцветы скоро пропадут. Тебе потребуется помощь!
 
+# findItems() возвращает массив предметов.
+items = hero.findItems()
 
-def CommandPeasant(soldier):
-    item = soldier.findNearestItem()
-    if item:
-        hero.command(soldier, "move", item.pos)
+# Получи первый самоцвет из массива.
+# Не забудь, что первый индекс равен 0.
+gem0 = items[0]
 
+# # Скажи Бруно получить gem0
+hero.say("Bruno " + gem0)
 
-while True:
-    commandTroops()
-    hero.move({'x': 41, 'y': 55})
+# Ты можешь ссылаться на самоцвет без переменной
+hero.say("Matilda " + items[1])
+
+# Создай переменную для последнего самоцвета, items[2]:
+gem2 = items[2]
+# Передвинься к этому самоцвету с помощью moveXY()
+hero.moveXY(gem2.pos.x, gem2.pos.y)
